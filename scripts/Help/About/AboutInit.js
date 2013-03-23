55aae358f28495e20c719443cc17aacbce46a0f2
Andrew Mustun <andrew@mustun.com>
2013-03-23 21:00:14 +0100

    fix path to icon

--- scripts/Help/About/AboutInit.js
+++ scripts/Help/About/AboutInit.js
@@ -2,7 +2,8 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("About", "&About..."), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(false);
     action.setScriptFile(basePath + "/About.js");
-    action.setIcon(basePath + "../../qcad_icon.svg");
+    qDebug("help about init: basePath: ", basePath);
+    action.setIcon(basePath + "/../../qcad_icon.svg");
     action.setSortOrder(999);
     action.setNoState();
     EAction.addGuiActionTo(action, Help, true, false, false, true);
