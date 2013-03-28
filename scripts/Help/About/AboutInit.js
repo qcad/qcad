fc36826c838adda9cec20a8df2d5c80c32509338
Andrew Mustun <andrew@qcad.org>
2013-03-28 09:27:33 +0100

    fix ref manual button css

--- scripts/Help/About/AboutInit.js
+++ scripts/Help/About/AboutInit.js
@@ -2,7 +2,6 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("About", "&About..."), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(false);
     action.setScriptFile(basePath + "/About.js");
-    qDebug("help about init: basePath: ", basePath);
     action.setIcon(basePath + "/../../qcad_icon.svg");
     action.setSortOrder(999);
     action.setNoState();
