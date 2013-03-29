595fe7394256d15329074552282f05566a4423af
Andrew Mustun <andrew@qcad.org>
2013-03-29 14:46:15 +0100

    add command for reset tool

--- scripts/Reset/ResetPostInit.js
+++ scripts/Reset/ResetPostInit.js
@@ -4,6 +4,7 @@ function postInit(basePath) {
     action.setScriptFile(basePath + "/Reset.js");
     action.setIcon(basePath + "/Reset.svg");
     action.setDefaultShortcut(new QKeySequence("q,q"));
+    action.setDefaultCommands(["reset", "qq"]);
     action.setSortOrder(10100);
     EAction.addGuiActionTo(action, Edit, true, false, false);
 
