92c019ca11ba694f09703f6611b291d95ea31f5f
Andrew Mustun <andrew@qcad.org>
2013-04-09 17:08:14 +0200

    update translations

---
+++ scripts/Edit/AppPreferences/TextPreferences/ts/TextPreferences_de.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>Text</name>
+    <message>
+        <location filename="../PreferencesPage.ui" line="17"/>
+        <source>Show font preview in font controls</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferencesPage.ui" line="27"/>
+        <source>Use reduced selection of fonts:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferencesPage.ui" line="41"/>
+        <source>Uncheck all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferencesPage.ui" line="48"/>
+        <source>Check all</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TextPreferences</name>
+    <message>
+        <location filename="../TextPreferences.js" line="30"/>
+        <source>Draw</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TextPreferences.js" line="30"/>
+        <source>Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
