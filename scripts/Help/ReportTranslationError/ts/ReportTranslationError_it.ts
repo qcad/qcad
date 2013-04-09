761ad8cc702930e411a3d6f812c55ec0e66d3c21
Andrew Mustun <andrew@qcad.org>
2013-04-09 10:17:12 +0200

    update translations

---
+++ scripts/Help/ReportTranslationError/ts/ReportTranslationError_it.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>ReportTranslationError</name>
+    <message>
+        <location filename="../ReportTranslationErrorInit.js" line="3"/>
+        <source>Report &amp;Translation Error</source>
+        <translation>Segnala errore di &amp;traduzione</translation>
+    </message>
+</context>
+</TS>
