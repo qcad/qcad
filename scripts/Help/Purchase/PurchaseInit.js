a699e820fd8718f58412bef0111372273c07ba72
Andrew Mustun <andrew@qcad.org>
2013-05-13 14:21:22 +0200

    remove obsolete menu

--- scripts/Help/Purchase/PurchaseInit.js
+++
@@ -1,9 +0,0 @@
-function init(basePath) {
-    var action = new RGuiAction(qsTranslate("Purchase", "&Purchase %1 Extensions").arg(qApp.applicationName),
-        RMainWindowQt.getMainWindow());
-    action.setRequiresDocument(false);
-    action.setScriptFile(basePath + "/Purchase.js");
-    action.setSortOrder(1500);
-    action.setNoState();
-    EAction.addGuiActionTo(action, Help, true, false, false, true);
-}
