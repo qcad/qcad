68fe5edc9dacb88cf37ddc8fbf1aecf1788767ec
Andrew Mustun <andrew@qcad.org>
2013-08-09 23:55:00 +0200

    cleanup

--- src/3rdparty/opennurbs/opennurbs_system.h
+++ src/3rdparty/opennurbs/opennurbs_system.h
@@ -185,11 +185,6 @@
 
 #include <stdlib.h>
 #include <memory.h>
-//#if defined(ON_COMPILER_XCODE)
-//#include <malloc/malloc.h>
-//#else
-//#include <malloc.h>
-//#endif
 #include <string.h>
 #include <math.h>
 #include <stdio.h>
