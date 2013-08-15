2cce0616532982fe7b4559968a781940ab72b185
Andrew Mustun <andrew@qcad.org>
2013-08-15 14:39:45 +0200

    update translations

---
+++ scripts/Draw/Ellipse/EllipseInscribedQuad/ts/EllipseInscribedQuad_de.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>EllipseInscribedQuad</name>
+    <message>
+        <location filename="../EllipseInscribedQuad.js" line="82"/>
+        <source>First line</source>
+        <translation>Erste Linie</translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuad.js" line="96"/>
+        <source>Second line</source>
+        <translation>Zweite Linie</translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuad.js" line="109"/>
+        <source>Third line</source>
+        <translation>Dritte Linie</translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuad.js" line="119"/>
+        <source>Fourth line</source>
+        <translation>Vierte Linie</translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuadInit.js" line="2"/>
+        <source>Inscribed in Quad</source>
+        <translation>Eingeschrieben in Viereck</translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuadInit.js" line="7"/>
+        <source>Draw ellipse inscribed in quadrilateral</source>
+        <translation>Ellipse, eingeschrieben in Viereck (tangential zu vier Linien)</translation>
+    </message>
+</context>
+</TS>
---
+++ scripts/Draw/Ellipse/EllipseInscribedQuad/ts/EllipseInscribedQuad_es.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>EllipseInscribedQuad</name>
+    <message>
+        <location filename="../EllipseInscribedQuad.js" line="82"/>
+        <source>First line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuad.js" line="96"/>
+        <source>Second line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuad.js" line="109"/>
+        <source>Third line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuad.js" line="119"/>
+        <source>Fourth line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuadInit.js" line="2"/>
+        <source>Inscribed in Quad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EllipseInscribedQuadInit.js" line="7"/>
+        <source>Draw ellipse inscribed in quadrilateral</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
