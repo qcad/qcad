95b43b7c25c3e108f73d3566e4a54e597cde31fa
Andrew Mustun <andrew@qcad.org>
2013-09-09 18:22:06 +0200

    update doc, translation

---
+++ scripts/Draw/AttributeDefinition/ts/AttributeDefinition_ja.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>AttributeDefinition</name>
+    <message>
+        <location filename="../AttributeDefinitionInit.js" line="2"/>
+        <source>&amp;Attribute Definition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AttributeDefinitionInit.js" line="7"/>
+        <source>Add an attribute definition</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
