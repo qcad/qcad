23cbd10123da25d13de88a2dc430e24e75a1500f
Andrew Mustun <andrew@qcad.org>
2013-10-18 16:38:26 +0200

    snapshot version 3.3.4.2

--- src/core/RVersion.h
+++ src/core/RVersion.h
@@ -23,11 +23,11 @@
 /**
  * Version number of the QCAD Application Framework.
  */
-#define R_QCAD_VERSION_STRING "3.4.0"
+#define R_QCAD_VERSION_STRING "3.3.4.2"
 
 #define R_QCAD_VERSION_MAJOR    3
-#define R_QCAD_VERSION_MINOR    4
-#define R_QCAD_VERSION_REV      0
-#define R_QCAD_VERSION_BUILD    0
+#define R_QCAD_VERSION_MINOR    3
+#define R_QCAD_VERSION_REV      4
+#define R_QCAD_VERSION_BUILD    2
 
 #endif
