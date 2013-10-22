551deb889b66d7c52837d159a981803dbe492d40
Tamas TEVESZ <ice@extreme.hu>
2013-10-22 15:34:38 +0200

    De-allcaps my name.

--- scripts/Help/About/AboutCredits.js
+++ scripts/Help/About/AboutCredits.js
@@ -5,7 +5,7 @@ var credits =
         [ "Andrew Mustun", "Main developer." ],
         [ "Matthias Melcher", "Drawing box joint lines, drawing dovetail joint lines, MOL file import." ],
         [ "Robert S.", "Direct distance entry, Snap - X/Y from Points, Snap - Y/X from Points." ],
-        [ "Tamas TEVESZ ('kazmer')", "Port to FreeBSD, various improvements and fixes." ],
+        [ "Tamas Tevesz", "Port to FreeBSD, various improvements and fixes." ],
     ],
     [
         "Translations",
