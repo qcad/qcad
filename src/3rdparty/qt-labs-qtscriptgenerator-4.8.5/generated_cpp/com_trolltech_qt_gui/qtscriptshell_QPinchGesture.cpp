f7e821330fee32d119982c67bebd6668769560de
Andrew Mustun <andrew@qcad.org>
2013-10-26 20:09:26 +0200

    add pinch gesture support for script bindings of qt 4.8.5

---
+++ src/3rdparty/qt-labs-qtscriptgenerator-4.8.5/generated_cpp/com_trolltech_qt_gui/qtscriptshell_QPinchGesture.cpp
@@ -0,0 +1,92 @@
+#include "qtscriptshell_QPinchGesture.h"
+
+#include <QtScript/QScriptEngine>
+#include <QVariant>
+#include <qbytearray.h>
+#include <qcoreevent.h>
+#include <qlist.h>
+#include <qobject.h>
+#include <qpoint.h>
+
+#define QTSCRIPT_IS_GENERATED_FUNCTION(fun) ((fun.data().toUInt32() & 0xFFFF0000) == 0xBABE0000)
+
+Q_DECLARE_METATYPE(QChildEvent*)
+Q_DECLARE_METATYPE(QEvent*)
+Q_DECLARE_METATYPE(QTimerEvent*)
+
+QtScriptShell_QPinchGesture::QtScriptShell_QPinchGesture(QObject*  parent)
+    : QPinchGesture(parent) {}
+
+QtScriptShell_QPinchGesture::~QtScriptShell_QPinchGesture() {}
+
+void QtScriptShell_QPinchGesture::childEvent(QChildEvent*  arg__1)
+{
+    QScriptValue _q_function = __qtscript_self.property("childEvent");
+    if (!_q_function.isFunction() || QTSCRIPT_IS_GENERATED_FUNCTION(_q_function)
+        || (__qtscript_self.propertyFlags("childEvent") & QScriptValue::QObjectMember)) {
+        QPinchGesture::childEvent(arg__1);
+    } else {
+        QScriptEngine *_q_engine = __qtscript_self.engine();
+        _q_function.call(__qtscript_self,
+            QScriptValueList()
+            << qScriptValueFromValue(_q_engine, arg__1));
+    }
+}
+
+void QtScriptShell_QPinchGesture::customEvent(QEvent*  arg__1)
+{
+    QScriptValue _q_function = __qtscript_self.property("customEvent");
+    if (!_q_function.isFunction() || QTSCRIPT_IS_GENERATED_FUNCTION(_q_function)
+        || (__qtscript_self.propertyFlags("customEvent") & QScriptValue::QObjectMember)) {
+        QPinchGesture::customEvent(arg__1);
+    } else {
+        QScriptEngine *_q_engine = __qtscript_self.engine();
+        _q_function.call(__qtscript_self,
+            QScriptValueList()
+            << qScriptValueFromValue(_q_engine, arg__1));
+    }
+}
+
+bool  QtScriptShell_QPinchGesture::event(QEvent*  arg__1)
+{
+    QScriptValue _q_function = __qtscript_self.property("event");
+    if (!_q_function.isFunction() || QTSCRIPT_IS_GENERATED_FUNCTION(_q_function)
+        || (__qtscript_self.propertyFlags("event") & QScriptValue::QObjectMember)) {
+        return QPinchGesture::event(arg__1);
+    } else {
+        QScriptEngine *_q_engine = __qtscript_self.engine();
+        return qscriptvalue_cast<bool >(_q_function.call(__qtscript_self,
+            QScriptValueList()
+            << qScriptValueFromValue(_q_engine, arg__1)));
+    }
+}
+
+bool  QtScriptShell_QPinchGesture::eventFilter(QObject*  arg__1, QEvent*  arg__2)
+{
+    QScriptValue _q_function = __qtscript_self.property("eventFilter");
+    if (!_q_function.isFunction() || QTSCRIPT_IS_GENERATED_FUNCTION(_q_function)
+        || (__qtscript_self.propertyFlags("eventFilter") & QScriptValue::QObjectMember)) {
+        return QPinchGesture::eventFilter(arg__1, arg__2);
+    } else {
+        QScriptEngine *_q_engine = __qtscript_self.engine();
+        return qscriptvalue_cast<bool >(_q_function.call(__qtscript_self,
+            QScriptValueList()
+            << qScriptValueFromValue(_q_engine, arg__1)
+            << qScriptValueFromValue(_q_engine, arg__2)));
+    }
+}
+
+void QtScriptShell_QPinchGesture::timerEvent(QTimerEvent*  arg__1)
+{
+    QScriptValue _q_function = __qtscript_self.property("timerEvent");
+    if (!_q_function.isFunction() || QTSCRIPT_IS_GENERATED_FUNCTION(_q_function)
+        || (__qtscript_self.propertyFlags("timerEvent") & QScriptValue::QObjectMember)) {
+        QPinchGesture::timerEvent(arg__1);
+    } else {
+        QScriptEngine *_q_engine = __qtscript_self.engine();
+        _q_function.call(__qtscript_self,
+            QScriptValueList()
+            << qScriptValueFromValue(_q_engine, arg__1));
+    }
+}
+
