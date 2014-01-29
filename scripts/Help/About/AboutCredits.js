6b67d127b60af45bc2bd81637d54a430ebcbec0e
Andrew Mustun <andrew@qcad.org>
2014-01-29 10:17:11 +0100

    update credits

--- scripts/Help/About/AboutCredits.js
+++ scripts/Help/About/AboutCredits.js
@@ -2,15 +2,19 @@ var credits =
 [
     [
         "Programming",
-        [ "Andrew Mustun",    "Main developer." ],
+        [ "Andrew Mustun",    "Main developer." 
+                              ],
         [ "Matthias Melcher", "Drawing box joint lines, " +
                               "drawing dovetail joint lines, " +
-                              "MOL file import." ],
+                              "MOL file import." 
+                              ],
         [ "Robert S.",        "Direct distance entry, " +
                               "Snap > X/Y from Points, " +
                               "Snap > Y/X from Points, " +
                               "Snap > Distance Manual, " +
-                              "Misc > Drawing > Centerline Cross"],
+                              "Misc > Drawing > Centerline Cross, " +
+                              "Draw > Line > Line from 2 Points (LX)"
+                              ],
         [ "Tamas Tevesz",     "Port to FreeBSD, " +
                               "various improvements and fixes." ],
     ],
