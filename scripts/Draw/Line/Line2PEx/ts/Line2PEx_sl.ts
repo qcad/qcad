e6b6eb6242a0808648341457179dd67e0ab2313c
miharix <miharix@gmail.com>
2014-01-30 22:27:40 +0100

    Slovenian translation of Line2PEx

--- scripts/Draw/Line/Line2PEx/ts/Line2PEx_sl.ts
+++ scripts/Draw/Line/Line2PEx/ts/Line2PEx_sl.ts
@@ -6,140 +6,140 @@
     <message>
         <location filename="../Line2PEx.js" line="87"/>
         <source>First point</source>
-        <translation type="unfinished"></translation>
+        <translation>Prva točka</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="94"/>
         <location filename="../Line2PEx.js" line="104"/>
         <location filename="../Line2PEx.js" line="107"/>
         <source>Next point</source>
-        <translation type="unfinished"></translation>
+        <translation>Naslednja točka</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="95"/>
         <source>Close/</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaključi/</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="96"/>
         <source>Undo</source>
-        <translation type="unfinished"></translation>
+        <translation>Razveljavi</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="97"/>
         <source>/Redo</source>
-        <translation type="unfinished"></translation>
+        <translation>/Uveljavi</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="99"/>
         <source>Redo</source>
-        <translation type="unfinished"></translation>
+        <translation>Razveljavi</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="102"/>
         <source>Next point or </source>
-        <translation type="unfinished"></translation>
+        <translation>Naslednja točka ali </translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="108"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Konec</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="422"/>
         <source>close</source>
-        <translation type="unfinished"></translation>
+        <translation>zapri</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="427"/>
         <source>undo</source>
-        <translation type="unfinished"></translation>
+        <translation>razveljavi</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="432"/>
         <source>redo</source>
-        <translation type="unfinished"></translation>
+        <translation>uveljavi</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="437"/>
         <source>t</source>
-        <translation type="unfinished"></translation>
+        <translation>t</translation>
     </message>
     <message>
         <location filename="../Line2PEx.js" line="444"/>
         <source>d</source>
-        <translation type="unfinished"></translation>
+        <translation>d</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="23"/>
         <source>Close Sequence</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaključi sekvenco</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="30"/>
         <source>C</source>
-        <translation type="unfinished"></translation>
+        <translation>Z</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="43"/>
         <source>Undo Previous Segment</source>
-        <translation type="unfinished"></translation>
+        <translation>Razveljavi prejšnji segment</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="50"/>
         <source>U</source>
-        <translation type="unfinished"></translation>
+        <translation>R</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="63"/>
         <source>Redo Line Segment</source>
-        <translation type="unfinished"></translation>
+        <translation>Uveljavi segment črte</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="70"/>
         <source>R</source>
-        <translation type="unfinished"></translation>
+        <translation>U</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="84"/>
         <location filename="../Line2PEx.ui" line="100"/>
         <source>Restrict to multiple of this angle</source>
-        <translation type="unfinished"></translation>
+        <translation>Omeji na večkratnik tega kota</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="87"/>
         <source>Restrict &amp;to multiple of</source>
-        <translation type="unfinished"></translation>
+        <translation>Omeji na &amp;večkratnik kota</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="90"/>
         <source>T</source>
-        <translation type="unfinished"></translation>
+        <translation>T</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="119"/>
         <source>degrees</source>
-        <translation type="unfinished"></translation>
+        <translation>stopinj</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="139"/>
         <source>&amp;Display Angles</source>
-        <translation type="unfinished"></translation>
+        <translation>Prikaži &amp;kote</translation>
     </message>
     <message>
         <location filename="../Line2PEx.ui" line="142"/>
         <source>D</source>
-        <translation type="unfinished"></translation>
+        <translation>D</translation>
     </message>
     <message>
         <location filename="../Line2PExInit.js" line="2"/>
         <source>Line from &amp;2 Points</source>
-        <translation type="unfinished"></translation>
+        <translation>Črta iz &amp;2 točk</translation>
     </message>
     <message>
         <location filename="../Line2PExInit.js" line="6"/>
         <source>Draw single line or sequence of lines</source>
-        <translation type="unfinished"></translation>
+        <translation>Nariši eno črto ali zaporedje črt</translation>
     </message>
 </context>
 </TS>
