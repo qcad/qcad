a78b12acf2beff67d9c4c6f5372b562ebfebbd42
Andrew Mustun <andrew@qcad.org>
2014-02-19 11:42:51 +0100

    update translations

--- scripts/Layer/HideAllLayers/ts/HideAllLayers_sl.ts
+++ scripts/Layer/HideAllLayers/ts/HideAllLayers_sl.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="sl_SI">
+<TS version="2.1" language="sl_SI">
 <context>
     <name>HideAllLayers</name>
     <message>
