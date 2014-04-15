502a23ea0c58c49823d697100edc8707052cdc6c
Andrew Mustun <andrew@qcad.org>
2014-04-15 17:05:38 +0200

    update to version 3.4.8

--- src/core/RVersion.h
+++ src/core/RVersion.h
@@ -23,11 +23,11 @@
 /**
  * Version number of the QCAD Application Framework.
  */
-#define R_QCAD_VERSION_STRING "3.4.7"
+#define R_QCAD_VERSION_STRING "3.4.8"
 
 #define R_QCAD_VERSION_MAJOR    3
 #define R_QCAD_VERSION_MINOR    4
-#define R_QCAD_VERSION_REV      7
+#define R_QCAD_VERSION_REV      8
 #define R_QCAD_VERSION_BUILD    0
 
 #endif
