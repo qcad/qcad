738e72f8385d7e307eacb731f2ee4d985216d0f7
Andrew Mustun <andrew@qcad.org>
2014-07-14 22:49:23 +0200

    add icons for apollonius tools (by blumi)

--- scripts/Misc/MiscDraw/LineRadicalAxis/LineRadicalAxisInit.js
+++ scripts/Misc/MiscDraw/LineRadicalAxis/LineRadicalAxisInit.js
@@ -2,7 +2,7 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("LineRadicalAxis", "Radical Axis of 2 Circles"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/LineRadicalAxis.js");
-    //action.setIcon(basePath + "/LineRadicalAxis.svg");
+    action.setIcon(basePath + "/LineRadicalAxis.svg");
     action.setSortOrder(300);
     EAction.addGuiActionTo(action, MiscDraw, true, true);
 }
