098a804d9b96c8eab104d30c3ed0b112004dea99
miharix <miharix@gmail.com>
2014-08-03 21:53:14 +0200

    Update translations

--- scripts/Misc/MiscSelect/ts/MiscSelect_sl.ts
+++ scripts/Misc/MiscSelect/ts/MiscSelect_sl.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="sl_SI">
+<TS version="2.0" language="sl_SI">
 <context>
     <name>MiscSelect</name>
     <message>
         <location filename="../MiscSelect.js" line="+62"/>
         <source>&amp;Selection</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Izbor</translation>
     </message>
 </context>
 </TS>
