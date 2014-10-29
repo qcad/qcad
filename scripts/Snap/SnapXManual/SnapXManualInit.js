c25f9f86a4ee3b7b66e5aeb6593c42988eabcf5b
Andrew Mustun <andrew@qcad.org>
2014-10-29 17:55:40 +0100

    add icons

--- scripts/Snap/SnapXManual/SnapXManualInit.js
+++ scripts/Snap/SnapXManual/SnapXManualInit.js
@@ -2,6 +2,7 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("SnapXManual", "&X/Y from Points"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/SnapXManual.js");
+    action.setIcon(basePath + "/SnapXManual.svg");
     action.setDefaultShortcut(new QKeySequence(".,x"));
     action.setDefaultCommands([".x"]);
     action.setSortOrder(3500);
--- scripts/Snap/SnapYManual/SnapYManualInit.js
+++ scripts/Snap/SnapYManual/SnapYManualInit.js
@@ -2,6 +2,7 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("SnapYManual", "&Y/X from Points"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/SnapYManual.js");
+    action.setIcon(basePath + "/SnapYManual.svg");
     action.setDefaultShortcut(new QKeySequence(".,y"));
     action.setDefaultCommands([".y"]);
     action.setSortOrder(3600);
