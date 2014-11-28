596eadd56e66a3d040c39c5396e072d2a4da75c6
Andrew Mustun <andrew@qcad.org>
2014-11-28 22:03:18 +0100

    add tool to reload linetypes from .lin for current drawing

---
+++ scripts/Misc/MiscModify/ReloadLinetypes/ReloadLinetypes.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2011-2014 by Andrew Mustun. All rights reserved.
+ * 
+ * This file is part of the QCAD project.
+ *
+ * QCAD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QCAD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QCAD.
+ */
+
+include("scripts/EAction.js");
+
+/**
+ * This action reloads all linetypes from
+ */
+function ReloadLinetypes(guiAction) {
+    EAction.call(this, guiAction);
+}
+
+ReloadLinetypes.prototype = new EAction();
+
+ReloadLinetypes.prototype.beginEvent = function() {
+    EAction.prototype.beginEvent.call(this);
+
+    var doc = this.getDocument();
+//    var transaction = new RTransaction(doc.getStorage(), "Reload linetypes", true);
+//    doc.initLinetypes(transaction);
+//    transaction.end();
+
+    var di = this.getDocumentInterface();
+
+    var lts = doc.getDefaultLinetypes();
+    var op = new RAddObjectsOperation();
+    for (var i=0; i<lts.length; i++) {
+        op.addObject(lts[i]);
+    }
+    di.applyOperation(op);
+
+    //di.regenerateScenes();
+
+    this.terminate();
+};
+
+ReloadLinetypes.init = function(basePath) {
+    var action = new RGuiAction(qsTr("Reload Linetype Definitions"), RMainWindowQt.getMainWindow());
+    action.setRequiresDocument(true);
+    action.setRequiresSelection(false);
+    action.setScriptFile(basePath + "/ReloadLinetypes.js");
+    action.setGroupSortOrder(56100);
+    action.setSortOrder(350);
+    action.setWidgetNames(["MiscModifyMenu", "MiscModifyToolBar", "MiscModifyToolsPanel"]);
+};
