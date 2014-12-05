4c5f32eaf355fc5496c200f243851d7c7dd78bc5
Andrew Mustun <andrew@qcad.org>
2014-12-05 11:49:10 +0100

    clean up

--- scripts/Draw/Shape/ShapeRectangleSize/ShapeRectangleSize.js
+++ scripts/Draw/Shape/ShapeRectangleSize/ShapeRectangleSize.js
@@ -39,17 +39,17 @@ function ShapeRectangleSize(guiAction) {
     this.width = 1;
     this.height = 1;
     
-    // [ObjectName], [Shown Text], offset vector, key code
+    // [ObjectName], [Shown Text], offset vector
     this.rbReferencePoints = [
-        [ "TopLeft",     qsTr("Top Left"),     new RVector(-1,  1), Qt.Key_1.valueOf() ],
-        [ "Top",         qsTr("Top"),          new RVector( 0,  1), Qt.Key_2.valueOf() ],
-        [ "TopRight",    qsTr("Top Right"),    new RVector( 1,  1), Qt.Key_3.valueOf() ],
-        [ "Left",        qsTr("Left"),         new RVector(-1,  0), Qt.Key_4.valueOf() ],
-        [ "Middle",      qsTr("Middle"),       new RVector( 0,  0), Qt.Key_5 ],
-        [ "Right",       qsTr("Right"),        new RVector( 1,  0), Qt.Key_6.valueOf() ],
-        [ "BottomLeft",  qsTr("Bottom Left"),  new RVector(-1, -1), Qt.Key_7.valueOf() ],
-        [ "Bottom",      qsTr("Bottom"),       new RVector( 0, -1), Qt.Key_8.valueOf() ],
-        [ "BottomRight", qsTr("Bottom Right"), new RVector( 1, -1), Qt.Key_9.valueOf() ]
+        [ "TopLeft",     qsTr("Top Left"),     new RVector(-1,  1) ],
+        [ "Top",         qsTr("Top"),          new RVector( 0,  1) ],
+        [ "TopRight",    qsTr("Top Right"),    new RVector( 1,  1) ],
+        [ "Left",        qsTr("Left"),         new RVector(-1,  0) ],
+        [ "Middle",      qsTr("Middle"),       new RVector( 0,  0) ],
+        [ "Right",       qsTr("Right"),        new RVector( 1,  0) ],
+        [ "BottomLeft",  qsTr("Bottom Left"),  new RVector(-1, -1) ],
+        [ "Bottom",      qsTr("Bottom"),       new RVector( 0, -1) ],
+        [ "BottomRight", qsTr("Bottom Right"), new RVector( 1, -1) ]
     ];
     this.referencePoint = this.rbReferencePoints[0][2];
 }
@@ -64,7 +64,7 @@ ShapeRectangleSize.prototype.showUiOptions = function(resume, restoreFromSetting
     var shortcuts = [];
 
     for (var i=0; i<this.rbReferencePoints.length; i++) {
-        var str = String.fromCharCode(this.rbReferencePoints[i][3]);
+        var str = "%1".arg(i+1);
         refPointCombo.addItem(
             "[" + str + "] " + this.rbReferencePoints[i][1],
             this.rbReferencePoints[i][2]
@@ -171,18 +171,7 @@ ShapeRectangleSize.prototype.slotReferencePointChanged = function(value) {
     this.updatePreview(true);
 };
 
-//ShapeRectangleSize.prototype.keyPressEvent = function(event) {
-//    var optionsToolBar = EAction.getOptionsToolBar();
-//    var refPointCombo = optionsToolBar.findChild("ReferencePoint");
 
-//    for (var i=0; i<this.rbReferencePoints.length; i++) {
-//        if (event.key()===this.rbReferencePoints[i][3]) {
-//            this.referencePoint = this.rbReferencePoints[i][2];
-//            refPointCombo.currentIndex = refPointCombo.findData(this.referencePoint);
-//        }
-//    }
-//    this.updatePreview(true);
-//};
 
 /**
  * Reacts to an assigned shortcut for the given index of the reference point combo box.
