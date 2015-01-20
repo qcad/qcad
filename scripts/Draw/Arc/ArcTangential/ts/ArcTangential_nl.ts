ddb69795be3f4f5ccd49bc3b2e9d9e96248f02a3
Andrew Mustun <andrew@qcad.org>
2015-01-20 14:47:06 +0100

    update dutch translation

--- scripts/Draw/Arc/ArcTangential/ts/ArcTangential_nl.ts
+++ scripts/Draw/Arc/ArcTangential/ts/ArcTangential_nl.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl_NL">
+<TS version="2.0" language="nl_NL">
 <context>
     <name>ArcTangential</name>
     <message>
         <location filename="../ArcTangential.js" line="+63"/>
         <source>Choose base line or base arc</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies lijn of boog</translation>
     </message>
     <message>
         <location line="+5"/>
         <source>Choose end point of arc</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies eindpunt van de boog</translation>
     </message>
     <message>
         <location filename="../ArcTangential.ui" line="+17"/>
         <source>Radius:</source>
-        <translation type="unfinished"></translation>
+        <translation>Straal:</translation>
     </message>
     <message>
         <location filename="../ArcTangentialInit.js" line="+2"/>
         <source>&amp;Tangentially Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Tangentiaal aansluitend</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Draw arc tangential to base entity</source>
-        <translation type="unfinished"></translation>
+        <translation>Tangentiaal aansluitend boog tekenen</translation>
     </message>
 </context>
 </TS>
--- scripts/Draw/Arc/ts/Arc_nl.ts
+++ scripts/Draw/Arc/ts/Arc_nl.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl_NL">
+<TS version="2.0" language="nl_NL">
 <context>
     <name>Arc</name>
     <message>
         <location filename="../Arc.js" line="+71"/>
         <source>Arc Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Boogen gereedschappen</translation>
     </message>
     <message>
         <location line="+5"/>
         <source>Show arc tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Toon bogen gereedschappen</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>&amp;Arc</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Boog</translation>
     </message>
 </context>
 </TS>
