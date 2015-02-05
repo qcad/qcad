8f239ced6e2e5226abd90b544b92a1fe073f2256
Andrew Mustun <andrew@qcad.org>
2015-02-05 11:29:42 +0100

    update simple api

--- scripts/simple.js
+++ scripts/simple.js
@@ -2,6 +2,59 @@
  * Simple API, mainly designed for use in the ECMAScript console.
  */
 
+include("scripts/library.js");
+
+PI = Math.PI;
+LN2 = Math.LN2;
+LN10 = Math.LN10;
+LOG2E = Math.LOG2E;
+LOG10E = Math.LOG10E;
+SQRT1_2 = Math.SQRT1_2;
+SQRT2 = Math.SQRT2;
+abs = Math.abs;
+ceil = Math.ceil;
+floor = Math.floor;
+exp = Math.exp;
+log = Math.log;
+max = Math.max;
+min = Math.min;
+pow = Math.pow;
+sqrt = Math.sqrt;
+random = Math.random;
+round = Math.round;
+
+function rad2deg(a) { 
+    return a / (2.0 * Math.PI) * 360.0; 
+}
+
+function deg2rad(a) { 
+    return (a / 360.0) * (2.0 * Math.PI); 
+}
+
+function sin(v) { 
+    return Math.sin(deg2rad(v)); 
+}
+
+function cos(v) { 
+    return Math.cos(deg2rad(v)); 
+}
+
+function tan(v) { 
+    return Math.tan(deg2rad(v)); 
+}
+
+function asin(v) { 
+    return rad2deg(Math.asin(v)); 
+}
+
+function acos(v) {
+    return rad2deg(Math.acos(v)); 
+}
+
+function atan(v) { 
+    return rad2deg(Math.atan(v)); 
+}
+
 function getMainWindow() {
     return RMainWindowQt.getMainWindow();
 }
@@ -30,6 +83,26 @@ function addArc(cx, cy, radius, a1, a2, rev) {
 
 function addCircle(cx, cy, radius) {
     addShape(new RCircle(cx, cy, radius));
+}
+
+/**
+ * \param points Array of RVector or [x,y] tuples.
+ */
+function addPolyline(points, closed) {
+    if (isNull(closed)) {
+        closed = false;
+    }
+    var pl = new RPolyline();
+    pl.setClosed(closed);
+    for (var i=0; i<points.length; i++) {
+        if (isVector(points[i])) {
+            pl.appendVertex(points[i]);
+        }
+        else {
+            pl.appendVertex(new RVector(points[i][0], points[i][1]));
+        }
+    }
+    addShape(pl);
 }
 
 function addSimpleText(text, x, y, height, angle, font, vAlign, hAlign, bold, italic) {
