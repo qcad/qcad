ae109a0097079ed654c684d2ed9947d672a5fda1
Andrew Mustun <andrew@qcad.org>
2015-03-31 10:49:20 +0200

    add File > Print Current View

---
+++ scripts/File/PrintCurrentView/ts/PrintCurrentView_fi.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fi">
+<context>
+    <name>Print</name>
+    <message>
+        <source>&amp;Print</source>
+        <translation type="obsolete">&amp;Tulosta</translation>
+    </message>
+    <message>
+        <source>Print current drawing</source>
+        <translation type="obsolete">Tulosta nykyinen piirros</translation>
+    </message>
+</context>
+<context>
+    <name>PrintCurrentView</name>
+    <message>
+        <location filename="../PrintCurrentViewInit.js" line="+2"/>
+        <source>Print &amp;Current View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Print current view</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ scripts/File/PrintCurrentView/ts/PrintCurrentView_pl.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>Print</name>
+    <message>
+        <source>&amp;Print</source>
+        <translatorcomment>--exact match--
+--file: qt_pl.ts--
+Wy&amp;drukuj...
+Wy&amp;drukuj</translatorcomment>
+        <translation type="obsolete">Wy&amp;drukuj</translation>
+    </message>
+    <message>
+        <source>Print current drawing</source>
+        <translatorcomment>--google--</translatorcomment>
+        <translation type="obsolete">Drukuj bieżący rysunek</translation>
+    </message>
+</context>
+<context>
+    <name>PrintCurrentView</name>
+    <message>
+        <location filename="../PrintCurrentViewInit.js" line="+2"/>
+        <source>Print &amp;Current View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Print current view</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
