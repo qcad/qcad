44b27f6d07f89ecb1189ad22c9311d33fe749906
Andrew Mustun <andrew@qcad.org>
2015-04-08 20:09:27 +0200

    fix for msvc / win / qt 4.7

--- scripts/Draw/DrawBasedOnRectangle.js
+++ scripts/Draw/DrawBasedOnRectangle.js
@@ -112,16 +112,16 @@ DrawBasedOnRectangle.prototype.showDialog = function() {
         var formLayout = this.dialog.findChild("FormLayout");
         for (i = 0; i < widgets.length; i++) {
             if (isOfType(widgets[i], QCheckBox)) {
-                this.moveWidget(widgets[i], this.dialog);
                 formLayout.addRow(new QWidget(formLayout), widgets[i]);
+                this.moveWidget(widgets[i], this.dialog);
                 continue;
             }
 
             // other widgets are moved to the dialog:
             if (i<widgets.length-1) {
+                formLayout.addRow(widgets[i], widgets[i+1]);
                 this.moveWidget(widgets[i], this.dialog);
                 this.moveWidget(widgets[i+1], this.dialog);
-                formLayout.addRow(widgets[i], widgets[i+1]);
                 i++;
             }
         }
