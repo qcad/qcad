34367ae0aa245a2fdea2ef9039111ffa3d13e8bd
Andrew Mustun <andrew@qcad.org>
2015-06-26 15:00:28 +0200

    update about dialog

--- scripts/Help/About/About.js
+++ scripts/Help/About/About.js
@@ -100,7 +100,7 @@ About.prototype.initAboutApp = function(webView) {
             + "<p/>"
             + "<p>%1 is free (open source) software.<br/>".arg(qApp.applicationName)
             + "This means that everyone can <a href='http://www.qcad.org/contribute'>get involved!</a></p>"
-            + "<p>Plugins and script add-ons are subject to their respective license (see 'About Plugins' tab).</p>"
+            + "<p>Plugins and script add-ons are subject to their respective license (see 'Plugins' tab).</p>"
             + "<p/>"
             + "<p>Internet: <a href='http://%1'>%1</a></p>".arg(qApp.organizationDomain)
             + "<p/>"
