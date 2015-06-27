9a4e547047e19cc4ac82ea756e0f21bc9fd2d324
Andrew Mustun <andrew@qcad.org>
2015-06-27 21:20:31 +0200

    port to linux

--- support/ecmagenerator/cpp2srcml.sh
+++ support/ecmagenerator/cpp2srcml.sh
@@ -57,7 +57,12 @@ do
 			echo "processing $cppfile ..."
 			(
 				#src2srcml "$f" $switch "$SPATH/$srcmlfile"
-				DYLD_LIBRARY_PATH=$srcmlpath srcml "$f" $switch "$SPATH/$srcmlfile"
+                if [ `uname` == "Darwin" ]
+                then
+                    DYLD_LIBRARY_PATH=$srcmlpath srcml "$f" $switch "$SPATH/$srcmlfile"
+                else
+                    LD_LIBRARY_PATH=$srcmlpath srcml "$f" $switch "$SPATH/$srcmlfile"
+                fi
 				#tidy -q -i -xml -m "$SPATH/$srcmlfile" # tidy messes up things
 			) &
 		#fi
