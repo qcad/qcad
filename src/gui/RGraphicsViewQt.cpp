71c9116438059c67e8f956ccd2744f78b9ebbc3f
Andrew Mustun <andrew@qcad.org>
2015-07-20 20:58:46 +0200

    updating graphics view for qt 4 / 5

--- src/gui/RGraphicsViewQt.cpp
+++ src/gui/RGraphicsViewQt.cpp
@@ -69,10 +69,12 @@ RGraphicsViewQt::~RGraphicsViewQt() {
  * Triggers a paintEvent based on a buffered offscreen bitmap (very fast).
  */
 void RGraphicsViewQt::repaintView() {
-    //update();
-
+#if QT_VERSION < 0x040800
     // 20150717: experimental: update is unreliable
     QWidget::repaint();
+#else
+    update();
+#endif
 }
 
 void RGraphicsViewQt::repaintNow() {
