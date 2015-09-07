77a4ba9e95e05d6aedb82fd06e37fff82e5e2abd
Andrew Mustun <andrew@qcad.org>
2015-09-07 17:13:52 +0200

    add polyfills for ECMAScript 2015 (ES6)

--- scripts/input.js
+++ scripts/input.js
@@ -39,5 +39,25 @@ acos = function(v) { return rad2deg(Math.acos(v)); };
 atan = function(v) { return rad2deg(Math.atan(v)); };
 atan2 = function(y,x) { return rad2deg(Math.atan2(y,x)); };
 
-// polyfills:
+
+// polyfills for ECMAScript 2015 (ES6):
 log10 = Math.log10 = function(x) { return Math.log(x) / Math.LN10; };
+log1p = Math.log1p = function(x) { return Math.log(1 + x); };
+log2 = Math.log2 = function(x) { return Math.log(x) / Math.LN2; };
+
+sign = Math.sign = function(x) { x = +x; if (x === 0 || isNaN(x)) { return x; } return x > 0 ? 1 : -1; };
+
+cosh = Math.cosh = function(x) { return (Math.exp(x) + Math.exp(-x)) / 2; };
+sinh = Math.sinh = function(x) { return (Math.exp(x) - Math.exp(-x)) / 2; };
+tanh = Math.tanh = function(x) { if (x === Infinity) { return 1; } else if (x === -Infinity) { return -1; } else { return (Math.exp(x) - Math.exp(-x)) / (Math.exp(x) + Math.exp(-x)); } };
+
+acosh = Math.acosh = function(x) { return Math.log(x + Math.sqrt(x * x - 1)); };
+asinh = Math.asinh = function(x) { if (x === -Infinity) { return x; } else { return Math.log(x + Math.sqrt(x * x + 1)); } };
+atanh = Math.atanh = function(x) { return Math.log((1+x)/(1-x)) / 2; };
+
+expm1 = Math.expm1 = function(x) { return Math.exp(x) - 1; };
+hypot = Math.hypot = function() { var y = 0; var length = arguments.length; for (var i = 0; i < length; i++) { if (arguments[i] === Infinity || arguments[i] === -Infinity) { return Infinity; } y += arguments[i] * arguments[i]; } return Math.sqrt(y); };
+
+cbrt = Math.cbrt = function(x) { var y = Math.pow(Math.abs(x), 1/3); return x < 0 ? -y : y; };
+
+trunc = Math.trunc = function(x) { return x < 0 ? Math.ceil(x) : Math.floor(x); };
