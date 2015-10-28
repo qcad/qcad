7f199c8f1e4953431cfde5493827bfcbb7267b45
Andrew Mustun <andrew@qcad.org>
2015-10-28 14:14:08 +0100

    refactor hatch creation for external use

--- scripts/Draw/Hatch/Hatch.js
+++ scripts/Draw/Hatch/Hatch.js
@@ -34,62 +34,49 @@ include("HatchDialog.js");
  */
 function Hatch(guiAction) {
     Draw.call(this, guiAction);
-
-    this.errorPoint = undefined;
 }
 
 Hatch.prototype = new Draw();
-Hatch.Tolerance = 0.001;
+Hatch.tolerance = 0.001;
+Hatch.traversed = {};
+Hatch.errorPoint = undefined;
 
 Hatch.prototype.beginEvent = function() {
     var i, entityId, entity, shape;
     
     Draw.prototype.beginEvent.call(this);
 
+    var doc = this.getDocument();
+    var di = this.getDocumentInterface();
+
     // analyze selection:
-    var document = EAction.getDocument();
-    this.selectedIds = document.querySelectedEntities();
+    var selectedIds = doc.querySelectedEntities();
 
     // no selection:
-    if (this.selectedIds.length===0) {
+    if (selectedIds.length===0) {
         EAction.handleUserWarning(qsTr("No boundary selected."));
         this.terminate();
         return;
     }
 
-    // verify boundary entities:
-    for (i = 0; i < this.selectedIds.length; i++) {
-        entityId = this.selectedIds[i];
-        entity = document.queryEntityDirect(entityId);
-        if (!this.verifyBoundaryEntity(entity)) {
-            this.terminate();
-            return;
-        }
+    if (!Hatch.verifyBoundary(doc, selectedIds)) {
+        this.terminate();
+        return;
     }
 
     // collect hatch data (boundary elements ordered as loops):
-    this.hatchData = new RHatchData();
-    this.hatchData.setDocument(document);
-    document.traversed = {};
+    var hatchData = Hatch.createHatchData(doc, selectedIds);
 
-    for (i = 0; i < this.selectedIds.length; i++) {
-        entityId = this.selectedIds[i];
-        if (document.traversed[entityId]) {
-            continue;
-        }
-        entity = document.queryEntityDirect(entityId);
-        if (!this.traverse(entity, document)) {
-            this.updatePreview();
-            var di = EAction.getDocumentInterface();
-            di.keepPreview();
-            EAction.handleUserWarning(
-                        qsTr("Loop not closed at point %1/%2.")
-                        .arg(isNull(this.connectionPoint) ? "?" : this.connectionPoint.x)
-                        .arg(isNull(this.connectionPoint) ? "?" : this.connectionPoint.y),
-                        true);
-            this.terminate();
-            return;
-        }
+    if (isNull(hatchData)) {
+        this.updatePreview();
+        di.keepPreview();
+        EAction.handleUserWarning(
+                    qsTr("Loop not closed at point %1/%2.")
+                    .arg(isNull(Hatch.connectionPoint) ? "?" : Hatch.connectionPoint.x)
+                    .arg(isNull(Hatch.connectionPoint) ? "?" : Hatch.connectionPoint.y),
+                    true);
+        this.terminate();
+        return;
     }
 
     // show hatch dialog for hatch pattern parameters:
@@ -100,28 +87,59 @@ Hatch.prototype.beginEvent = function() {
         return;
     }
 
-    this.hatchData.setSolid(hatchDataDlg.isSolid());
-    this.hatchData.setScale(hatchDataDlg.getScale());
-    this.hatchData.setAngle(hatchDataDlg.getAngle());
-    this.hatchData.setPatternName(hatchDataDlg.getPatternName());
+    hatchData.setSolid(hatchDataDlg.isSolid());
+    hatchData.setScale(hatchDataDlg.getScale());
+    hatchData.setAngle(hatchDataDlg.getAngle());
+    hatchData.setPatternName(hatchDataDlg.getPatternName());
 
-    var di = this.getDocumentInterface();
-    var hatch = new RHatchEntity(
-        this.getDocument(),
-        this.hatchData
-    );
+    var hatch = new RHatchEntity(doc, hatchData);
     var op = new RAddObjectOperation(hatch, this.getToolTitle());
     di.applyOperation(op);
 
     this.terminate();
 };
 
+Hatch.prototype.getAuxPreview = function() {
+    if (!isValidVector(Hatch.errorPoint)) {
+        return undefined;
+    }
 
-Hatch.prototype.verifyBoundaryEntity = function(entity) {
+    var ret = [];
+
+    var view = EAction.getGraphicsView();
+    if (isNull(view)) {
+        return undefined;
+    }
+
+    var radius = view.mapDistanceFromView(10.0);
+
+    ret.push(new RCircle(Hatch.errorPoint, radius));
+
+    return ret;
+};
+
+/**
+ * Verifies the entities entityIds as potential hatch boundary.
+ */
+Hatch.verifyBoundary = function(doc, entityIds) {
+    for (var i = 0; i < entityIds.length; i++) {
+        var entityId = entityIds[i];
+        var entity = doc.queryEntityDirect(entityId);
+        if (!Hatch.verifyBoundaryEntity(doc, entity)) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+/**
+ * Verifies the given entity as potential hatch boundary.
+ */
+Hatch.verifyBoundaryEntity = function(doc, entity) {
     if (isBlockReferenceEntity(entity)) {
-        var document = EAction.getDocument();
         var blockReferenceData = entity.getData();
-        var ids = document.queryBlockEntities(blockReferenceData.getReferencedBlockId());
+        var ids = doc.queryBlockEntities(blockReferenceData.getReferencedBlockId());
         var ret = true;
         for (var i=0; i<ids.length; i++) {
             var bEntity = blockReferenceData.queryEntity(ids[i]);
@@ -131,7 +149,7 @@ Hatch.prototype.verifyBoundaryEntity = function(entity) {
             if (!bEntity.isVisible()) {
                 continue;
             }
-            ret = ret && this.verifyBoundaryEntity(bEntity);
+            ret = ret && Hatch.verifyBoundaryEntity(bEntity);
         }
         return ret;
     }
@@ -144,7 +162,6 @@ Hatch.prototype.verifyBoundaryEntity = function(entity) {
         isSplineEntity(entity)) {
 
         return true;
-
     }
 
     EAction.handleUserWarning(
@@ -156,40 +173,53 @@ Hatch.prototype.verifyBoundaryEntity = function(entity) {
     return false;
 };
 
-Hatch.prototype.isClosedPolyline = function(entity) {
-    return isPolylineEntity(entity) && entity.isGeometricallyClosed();
-};
+/**
+ * \return RHatchData object created from the given boundary entities entityIds or undefined.
+ */
+Hatch.createHatchData = function(doc, entityIds) {
+    var hatchData = new RHatchData();
+    hatchData.setDocument(doc);
 
-Hatch.prototype.isClosedCurve = function(entity) {
-    return isCircleEntity(entity) ||
-            (isEllipseEntity(entity) && entity.isFullEllipse()) ||
-            (isSplineEntity(entity) && entity.isClosed());
-};
+    Hatch.traversed = {};
+    Hatch.errorPoint = undefined;
 
-Hatch.prototype.traverse = function(entity, entitySource, candidateIds) {
-    var i;
-
-    if (isNull(candidateIds)) {
-        candidateIds = this.selectedIds;
+    for (var i=0; i<entityIds.length; i++) {
+        var entityId = entityIds[i];
+        if (Hatch.traversed[entityId]) {
+            continue;
+        }
+        var entity = doc.queryEntityDirect(entityId);
+        if (!Hatch.traverse(hatchData, doc, entity, entityIds)) {
+            return undefined;
+        }
     }
 
+    return hatchData;
+};
+
+/**
+ * Traverses the given candidates recursively for connected entities.
+ */
+Hatch.traverse = function(hatchData, docOrBlockRef, entity, candidateIds) {
+    var i;
+
     // handle closed polyline loops:
-    if (this.isClosedPolyline(entity)) {
-        this.hatchData.newLoop();
+    if (Hatch.isClosedPolyline(entity)) {
+        hatchData.newLoop();
         var segments = entity.getExploded();
         for (i=0; i<segments.length; i++) {
             var segment = segments[i];
-            this.hatchData.addBoundary(segment);
+            hatchData.addBoundary(segment);
         }
-        entitySource.traversed[entity.getId()] = true;
+        Hatch.traversed[entity.getId()] = true;
         return true;
     }
 
     // handle circle, full ellipse and closed spline loops:
-    if (this.isClosedCurve(entity)) {
-        this.hatchData.newLoop();
-        this.hatchData.addBoundary(entity.getData().castToShape());
-        entitySource.traversed[entity.getId()] = true;
+    if (Hatch.isClosedCurve(entity)) {
+        hatchData.newLoop();
+        hatchData.addBoundary(entity.getData().castToShape());
+        Hatch.traversed[entity.getId()] = true;
         return true;
     }
 
@@ -198,7 +228,7 @@ Hatch.prototype.traverse = function(entity, entitySource, candidateIds) {
     // handle block reference:
     if (isBlockReferenceEntity(entity)) {
         var blockReferenceData = entity.getData();
-        blockReferenceData.traversed = new Object();
+        blockReferenceData.traversed = {};
         var ids = document.queryBlockEntities(blockReferenceData.getReferencedBlockId());
         var ret = true;
         for (i=0; i<ids.length; i++) {
@@ -213,99 +243,87 @@ Hatch.prototype.traverse = function(entity, entitySource, candidateIds) {
             if (!bEntity.isVisible()) {
                 continue;
             }
-            this.traverse(bEntity, blockReferenceData, ids);
+            Hatch.traverse(hatchData, blockReferenceData, bEntity, ids);
         }
         return ret;
     }
 
     // handle 'loose' boundary elements:
-
-    var currentShape;
-    var loopStartPoint;
-
-    this.hatchData.newLoop();
-    entitySource.traversed[entity.getId()] = true;
+    hatchData.newLoop();
+    Hatch.traversed[entity.getId()] = true;
     var shape = entity.getData().castToShape().clone();
-    this.hatchData.addBoundary(shape);
-    currentShape = shape;
-    loopStartPoint = shape.getStartPoint();
+    hatchData.addBoundary(shape);
+    var currentShape = shape;
+    var loopStartPoint = shape.getStartPoint();
+
     // find connected entities:
     var done2 = true;
-    this.connectionPoint = undefined;
+    Hatch.connectionPoint = undefined;
     do {
         done2 = true;
-        this.connectionPoint = currentShape.getEndPoint();
+        Hatch.connectionPoint = currentShape.getEndPoint();
         for (i = 0; i < candidateIds.length; i++) {
             var entityId = candidateIds[i];
-            if (entitySource.traversed[entityId]) {
+            if (Hatch.traversed[entityId]) {
                 continue;
             }
 
-            if (isFunction(entitySource.queryEntityDirect)) {
+            if (isFunction(docOrBlockRef.queryEntityDirect)) {
                 // query entity from document:
-                entity = entitySource.queryEntityDirect(entityId);
+                entity = docOrBlockRef.queryEntityDirect(entityId);
             }
             else {
                 // query entity from block reference:
-                entity = entitySource.queryEntity(entityId);
+                entity = docOrBlockRef.queryEntity(entityId);
             }
 
             if (isBlockReferenceEntity(entity)) {
                 continue;
             }
 
-            if (this.isClosedCurve(entity) || this.isClosedPolyline(entity)) {
+            if (Hatch.isClosedCurve(entity) || Hatch.isClosedPolyline(entity)) {
                 continue;
             }
 
             var sp = entity.getStartPoint();
             var ep = entity.getEndPoint();
 
-            var spConnects = this.connectionPoint.equalsFuzzy(sp, Hatch.Tolerance);
-            var epConnects = this.connectionPoint.equalsFuzzy(ep, Hatch.Tolerance);
+            var spConnects = Hatch.connectionPoint.equalsFuzzy(sp, Hatch.tolerance);
+            var epConnects = Hatch.connectionPoint.equalsFuzzy(ep, Hatch.tolerance);
 
             if (spConnects || epConnects) {
-                entitySource.traversed[entityId] = true;
+                Hatch.traversed[entityId] = true;
                 shape = entity.getData().castToShape().clone();
                 if (epConnects) {
                     shape.reverse();
                 }
-                this.hatchData.addBoundary(shape);
+                hatchData.addBoundary(shape);
                 currentShape = shape;
                 done2 = false;
                 //qDebug("Hatch: next loop shape: ", shape, epConnects ? " (reversed)" : "");
                 break;
             }
 
-            //qDebug("gap sp: ", this.connectionPoint.getDistanceTo(sp));
-            //qDebug("gap ep: ", this.connectionPoint.getDistanceTo(ep));
+            //qDebug("gap sp: ", Hatch.connectionPoint.getDistanceTo(sp));
+            //qDebug("gap ep: ", Hatch.connectionPoint.getDistanceTo(ep));
         }
-    } while(!done2);
+    } while (!done2);
 
-    if (!this.connectionPoint.equalsFuzzy(loopStartPoint, Hatch.Tolerance)) {
-        this.errorPoint = this.connectionPoint;
-        //qDebug("start does not connect to end by: ", this.connectionPoint.getDistanceTo(loopStartPoint));
+    if (!Hatch.connectionPoint.equalsFuzzy(loopStartPoint, Hatch.tolerance)) {
+        Hatch.errorPoint = Hatch.connectionPoint;
+        //qDebug("start does not connect to end by: ", Hatch.connectionPoint.getDistanceTo(loopStartPoint));
         return false;
     }
 
     return true;
 };
 
-Hatch.prototype.getAuxPreview = function() {
-    if (!isValidVector(this.errorPoint)) {
-        return undefined;
-    }
+Hatch.isClosedPolyline = function(entity) {
+    return isPolylineEntity(entity) && entity.isGeometricallyClosed();
+};
 
-    var ret = [];
-
-    var view = EAction.getGraphicsView();
-    if (isNull(view)) {
-        return undefined;
-    }
-
-    var radius = view.mapDistanceFromView(10.0);
-
-    ret.push(new RCircle(this.errorPoint, radius));
-
-    return ret;
+Hatch.isClosedCurve = function(entity) {
+    return isCircleEntity(entity) ||
+            (isEllipseEntity(entity) && entity.isFullEllipse()) ||
+            (isSplineEntity(entity) && entity.isClosed());
 };
