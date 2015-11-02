a73b256b513bb09c6395eb6ab3eb0390f1d6b128
Andrew Mustun <andrew@qcad.org>
2015-11-02 11:39:39 +0100

    disable show dialog tool (breaks enter in line edits)

--- scripts/View/ShowToolDialog/ShowToolDialog.js
+++ scripts/View/ShowToolDialog/ShowToolDialog.js
@@ -28,24 +28,44 @@ ShowToolDialog.prototype = new EAction();
 ShowToolDialog.init = function(basePath) {
     var appWin = EAction.getMainWindow();
 
-    ShowToolDialog.shortcutReturn = new QShortcut(new QKeySequence(Qt.Key_Return.valueOf()), appWin, 0, 0, Qt.WindowShortcut);
-    ShowToolDialog.shortcutReturn.activated.connect(ShowToolDialog, "showDialog");
-    ShowToolDialog.shortcutReturn.activatedAmbiguously.connect(ShowToolDialog, "showDialog");
-    ShowToolDialog.shortcutEnter = new QShortcut(new QKeySequence(Qt.Key_Enter.valueOf()), appWin, 0, 0, Qt.WindowShortcut);
-    ShowToolDialog.shortcutEnter.activated.connect(ShowToolDialog, "showDialog");
-    ShowToolDialog.shortcutEnter.activatedAmbiguously.connect(ShowToolDialog, "showDialog");
+//    // TODO: breaks enter in command line
+//    //var context = Qt.WidgetShortcut;
+//    var context = Qt.WindowShortcut;
+//    ShowToolDialog.shortcutReturn = new QShortcut(new QKeySequence(Qt.Key_Return.valueOf()), appWin, 0, 0, context);
+//    ShowToolDialog.shortcutReturn.activated.connect(ShowToolDialog, "showDialog");
+//    ShowToolDialog.shortcutReturn.activatedAmbiguously.connect(ShowToolDialog, "showDialog");
+//    ShowToolDialog.shortcutEnter = new QShortcut(new QKeySequence(Qt.Key_Enter.valueOf()), appWin, 0, 0, context);
+//    ShowToolDialog.shortcutEnter.activated.connect(ShowToolDialog, "showDialog");
+//    ShowToolDialog.shortcutEnter.activatedAmbiguously.connect(ShowToolDialog, "showDialog");
+
+//    var action = new RGuiAction(qsTranslate("ShowToolDialog", "&Show Tool Dialog"), RMainWindowQt.getMainWindow());
+//    action.setScriptFile(basePath + "/ShowToolDialog.js");
+//    action.setDefaultShortcuts(
+//                [
+//                    new QKeySequence(Qt.Key_Return.valueOf()),
+//                    new QKeySequence(Qt.Key_Enter.valueOf()),
+//                ]);
+//    action.setNoState();
+//    action.checkable = false;
+//    action.setGroupSortOrder(3900);
+//    action.setSortOrder(150);
+//    action.setWidgetNames(["ViewMenu", "!ViewToolBar", "!ViewToolsPanel"]);
 };
 
 ShowToolDialog.showDialog = function() {
+//ShowToolDialog.prototype.beginEvent = function() {
     var di = EAction.getDocumentInterface();
     if (isNull(di)) {
+//        this.terminate();
         return;
     }
 
     var a = di.getCurrentAction();
     if (isNull(a)) {
+//        this.terminate();
         return;
     }
 
     a.showDialog();
+//    this.terminate();
 };
