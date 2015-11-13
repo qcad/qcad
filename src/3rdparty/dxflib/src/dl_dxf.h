d663b5353edeaf9627107e44fc74c3b462826cfb
Andrew Mustun <andrew@qcad.org>
2015-11-13 09:16:14 +0100

    set version of dxflib to 3.12.2

--- src/3rdparty/dxflib/src/dl_dxf.h
+++ src/3rdparty/dxflib/src/dl_dxf.h
@@ -57,11 +57,11 @@ class DL_CreationInterface;
 class DL_WriterA;
 
 
-#define DL_VERSION "3.7.5.0"
+#define DL_VERSION "3.12.2.0"
 
 #define DL_VERSION_MAJOR    3
-#define DL_VERSION_MINOR    7
-#define DL_VERSION_REV      5
+#define DL_VERSION_MINOR    12
+#define DL_VERSION_REV      2
 #define DL_VERSION_BUILD    0
 
 #define DL_UNKNOWN               0
