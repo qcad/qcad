c434fc67a221ac296ddf66a7fe9dd1d4819e178b
Andrew Mustun <andrew@qcad.org>
2015-11-25 12:13:04 +0100

    make more tools in misc menu translatable

---
+++ scripts/Misc/MiscModify/FlushBackLog/ts/FlushBackLog_sk.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk_SK">
+<context>
+    <name>FlushBackLog</name>
+    <message>
+        <location filename="../FlushBackLog.js" line="38"/>
+        <source>Flush Back Log</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FlushBackLog.js" line="39"/>
+        <source>Are you sure that you want to flush the undo / redo transaction history?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FlushBackLog.js" line="40"/>
+        <source>This cannot be undone.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../FlushBackLog.js" line="52"/>
+        <source>Flush &amp;Undo History</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ scripts/Misc/MiscModify/ReloadLinetypes/ts/ReloadLinetypes_sk.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk_SK">
+<context>
+    <name>ReloadLinetypes</name>
+    <message>
+        <location filename="../ReloadLinetypes.js" line="48"/>
+        <source>Reload Linetype Definitions</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
