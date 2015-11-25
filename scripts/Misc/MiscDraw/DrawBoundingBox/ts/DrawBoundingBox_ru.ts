fe88eb491197185924c7425d1ad8d2a9b0b8b840
Andrew Mustun <andrew@qcad.org>
2015-11-25 12:11:01 +0100

    make bounding box tool translatable

---
+++ scripts/Misc/MiscDraw/DrawBoundingBox/ts/DrawBoundingBox_ru.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>DrawBoundingBox</name>
+    <message>
+        <location filename="../DrawBoundingBox.js" line="71"/>
+        <source>Draw &amp;Bounding Box</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
