6958ce5f2195d057a515e45204a226ca301395f3
Andrew Mustun <andrew@qcad.org>
2015-12-10 20:41:22 +0100

    add tangential snap tool

---
+++ scripts/Snap/SnapTangential/SnapTangential.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2011-2015 by Andrew Mustun. All rights reserved.
+ * 
+ * This file is part of the QCAD project.
+ *
+ * QCAD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QCAD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QCAD.
+ */
+
+include("../Snap.js");
+
+/**
+ * \class SnapTangential
+ * \ingroup ecma_snap
+ * \brief Snap to point of tangency on entity.
+ */
+function SnapTangential(guiAction) {
+    Snap.call(this, guiAction);
+}
+
+SnapTangential.prototype = new Snap();
+
+SnapTangential.prototype.beginEvent = function() {
+    Snap.prototype.beginEvent.call(this);
+    this.getDocumentInterface().setSnap(new RSnapTangential());
+    this.terminate();
+};
+
---
+++ scripts/Snap/SnapTangential/SnapTangentialInit.js
@@ -0,0 +1,16 @@
+function init(basePath) {
+    var action = new RGuiAction(qsTranslate("SnapTangential", "&Tangential"), RMainWindowQt.getMainWindow());
+    action.setRequiresDocument(true);
+    action.setScriptFile(basePath + "/SnapTangential.js");
+    action.setIcon(basePath + "/SnapTangential.svg");
+    action.setDefaultShortcut(new QKeySequence("s,b"));
+    action.setDefaultCommands(["snaptangential", "sb"]);
+    action.setGroup("snaps");
+    action.setNoState(true);
+    action.setGroupSortOrder(15100);
+    action.setSortOrder(550);
+    action.setWidgetNames(["SnapMenu", "SnapToolBar", "SnapToolsPanel"]);
+
+    var appWin = EAction.getMainWindow();
+    appWin.addFocusListener(action);
+}
