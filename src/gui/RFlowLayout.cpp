b9a2618e31631cdb8c416c2a51695d2ec5e05b67
Andrew Mustun <andrew@qcad.org>
2016-01-21 09:06:02 +0100

    fix compile error in RFlowLayout

--- src/gui/RFlowLayout.cpp
+++ src/gui/RFlowLayout.cpp
@@ -3,98 +3,85 @@
 #include "RFlowLayout.h"
 #include "RSettings.h"
 
-RFlowLayout::RFlowLayout(QWidget *parent, int margin, int hSpacing, int vSpacing)
-    : QLayout(parent), m_hSpace(hSpacing), m_vSpace(vSpacing)
-{
+RFlowLayout::RFlowLayout(QWidget* parent, int margin, int hSpacing, int vSpacing)
+    : QLayout(parent), hSpace(hSpacing), vSpace(vSpacing) {
     setContentsMargins(margin, margin, margin, margin);
 }
 
 RFlowLayout::RFlowLayout(int margin, int hSpacing, int vSpacing)
-    : m_hSpace(hSpacing), m_vSpace(vSpacing)
-{
+    : hSpace(hSpacing), vSpace(vSpacing) {
     setContentsMargins(margin, margin, margin, margin);
 }
 
-RFlowLayout::~RFlowLayout()
-{
-    QLayoutItem *item;
+RFlowLayout::~RFlowLayout() {
+    QLayoutItem* item;
     while ((item = takeAt(0))) {
         delete item;
     }
 }
 
-void RFlowLayout::addItem(QLayoutItem *item)
-{
+void RFlowLayout::addItem(QLayoutItem* item) {
     itemList.append(item);
 }
 
-int RFlowLayout::horizontalSpacing() const
-{
-    if (m_hSpace >= 0) {
-        return m_hSpace;
+int RFlowLayout::horizontalSpacing() const {
+    if (hSpace >= 0) {
+        return hSpace;
     } else {
         return smartSpacing(QStyle::PM_LayoutHorizontalSpacing);
     }
 }
 
-int RFlowLayout::verticalSpacing() const
-{
-    if (m_vSpace >= 0) {
-        return m_vSpace;
+int RFlowLayout::verticalSpacing() const {
+    if (vSpace >= 0) {
+        return vSpace;
     } else {
         return smartSpacing(QStyle::PM_LayoutVerticalSpacing);
     }
 }
 
-int RFlowLayout::count() const
-{
+int RFlowLayout::count() const {
     return itemList.size();
 }
 
-QLayoutItem *RFlowLayout::itemAt(int index) const
-{
+QLayoutItem* RFlowLayout::itemAt(int index) const {
     return itemList.value(index);
 }
 
-QLayoutItem *RFlowLayout::takeAt(int index)
-{
-    if (index >= 0 && index < itemList.size())
+QLayoutItem* RFlowLayout::takeAt(int index) {
+    if (index >= 0 && index < itemList.size()) {
         return itemList.takeAt(index);
-    else
+    }
+    else {
         return 0;
+    }
 }
 
-Qt::Orientations RFlowLayout::expandingDirections() const
-{
+Qt::Orientations RFlowLayout::expandingDirections() const {
     return 0;
 }
 
-bool RFlowLayout::hasHeightForWidth() const
-{
+bool RFlowLayout::hasHeightForWidth() const {
     return true;
 }
 
-int RFlowLayout::heightForWidth(int width) const
-{
+int RFlowLayout::heightForWidth(int width) const {
     int height = doLayout(QRect(0, 0, width, 0), true);
     return height;
 }
 
-void RFlowLayout::setGeometry(const QRect &rect)
-{
+void RFlowLayout::setGeometry(const QRect& rect) {
     QLayout::setGeometry(rect);
     doLayout(rect, false);
 }
 
-QSize RFlowLayout::sizeHint() const
-{
+QSize RFlowLayout::sizeHint() const {
     return minimumSize();
 }
 
-QSize RFlowLayout::minimumSize() const
-{
+QSize RFlowLayout::minimumSize() const {
     QSize size;
-    QLayoutItem *item;
+    QLayoutItem* item;
     foreach (item, itemList) {
         size = size.expandedTo(item->minimumSize());
     }
@@ -103,8 +90,7 @@ QSize RFlowLayout::minimumSize() const
     return size;
 }
 
-int RFlowLayout::doLayout(const QRect &rect, bool testOnly) const
-{
+int RFlowLayout::doLayout(const QRect& rect, bool testOnly) const {
     int left, top, right, bottom;
     getContentsMargins(&left, &top, &right, &bottom);
     QRect effectiveRect = rect.adjusted(+left, +top, -right, -bottom);
@@ -112,9 +98,9 @@ int RFlowLayout::doLayout(const QRect &rect, bool testOnly) const
     int y = effectiveRect.y();
     int lineHeight = 0;
 
-    QLayoutItem *item;
+    QLayoutItem* item;
     foreach (item, itemList) {
-        QWidget *wid = item->widget();
+        QWidget* wid = item->widget();
 
         if (wid->isHidden()) {
             continue;
@@ -144,16 +130,16 @@ int RFlowLayout::doLayout(const QRect &rect, bool testOnly) const
     }
     return y + lineHeight - rect.y() + bottom;
 }
-int RFlowLayout::smartSpacing(QStyle::PixelMetric pm) const
-{
-    QObject *parent = this->parent();
+
+int RFlowLayout::smartSpacing(QStyle::PixelMetric pm) const {
+    QObject* parent = this->parent();
     if (!parent) {
         return -1;
     } else if (parent->isWidgetType()) {
-        QWidget *pw = static_cast<QWidget *>(parent);
+        QWidget* pw = static_cast<QWidget* >(parent);
         return pw->style()->pixelMetric(pm, 0, pw);
     } else {
-        return static_cast<QLayout *>(parent)->spacing();
+        return static_cast<QLayout* >(parent)->spacing();
     }
 }
 
@@ -179,7 +165,7 @@ void RFlowLayout::insertAction(int index, QAction* action) {
     itemList.insert(index, new QWidgetItem(button));
     invalidate();
 
-//    QToolBarItem *item = createItem(action);
+//    QToolBarItem* item = createItem(action);
 //    if (item) {
 //        items.insert(index, item);
 //        invalidate();
--- src/gui/RFlowLayout.h
+++ src/gui/RFlowLayout.h
@@ -4,37 +4,39 @@
 #include <QLayout>
 #include <QRect>
 #include <QStyle>
-class RFlowLayout : public QLayout
-{
+
+class RFlowLayout : public QLayout {
 public:
-    explicit RFlowLayout(QWidget *parent, int margin = -1, int hSpacing = -1, int vSpacing = -1);
+    explicit RFlowLayout(QWidget* parent, int margin = -1, int hSpacing = -1, int vSpacing = -1);
     explicit RFlowLayout(int margin = -1, int hSpacing = -1, int vSpacing = -1);
     ~RFlowLayout();
 
-    void addItem(QLayoutItem *item) Q_DECL_OVERRIDE;
+    void addItem(QLayoutItem* item);
     int horizontalSpacing() const;
     int verticalSpacing() const;
-    Qt::Orientations expandingDirections() const Q_DECL_OVERRIDE;
-    bool hasHeightForWidth() const Q_DECL_OVERRIDE;
-    int heightForWidth(int) const Q_DECL_OVERRIDE;
-    int count() const Q_DECL_OVERRIDE;
-    QLayoutItem *itemAt(int index) const Q_DECL_OVERRIDE;
-    QSize minimumSize() const Q_DECL_OVERRIDE;
-    void setGeometry(const QRect &rect) Q_DECL_OVERRIDE;
-    QSize sizeHint() const Q_DECL_OVERRIDE;
-    QLayoutItem *takeAt(int index) Q_DECL_OVERRIDE;
+    Qt::Orientations expandingDirections() const;
+    bool hasHeightForWidth() const;
+    int heightForWidth(int) const;
+    int count() const;
+    QLayoutItem* itemAt(int index) const;
+    QSize minimumSize() const;
+    void setGeometry(const QRect& rect);
+    QSize sizeHint() const;
+    QLayoutItem* takeAt(int index);
 
     void insertAction(int index, QAction* action);
     int indexOf(QAction* action) const;
-    int indexOf(QWidget* widget) const Q_DECL_OVERRIDE { return QLayout::indexOf(widget); }
+    int indexOf(QWidget* widget) const {
+        return QLayout::indexOf(widget);
+    }
 
 private:
-    int doLayout(const QRect &rect, bool testOnly) const;
+    int doLayout(const QRect& rect, bool testOnly) const;
     int smartSpacing(QStyle::PixelMetric pm) const;
 
-    QList<QLayoutItem *> itemList;
-    int m_hSpace;
-    int m_vSpace;
+    QList<QLayoutItem*> itemList;
+    int hSpace;
+    int vSpace;
 };
 
 #endif
