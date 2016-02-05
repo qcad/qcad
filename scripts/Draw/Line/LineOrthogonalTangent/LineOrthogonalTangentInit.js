88d9345acff5533a33ddbe6ccad679587a212c70
Andrew Mustun <andrew@qcad.org>
2016-02-05 13:57:47 +0100

    remove conflicting command

--- scripts/Draw/Line/LineOrthogonalTangent/LineOrthogonalTangentInit.js
+++ scripts/Draw/Line/LineOrthogonalTangent/LineOrthogonalTangentInit.js
@@ -5,7 +5,7 @@ function init(basePath) {
     action.setIcon(basePath + "/LineOrthogonalTangent.svg");
     action.setStatusTip(qsTranslate("LineOrthogonalTangent", "Draw line orthogonal to line and tangent to circle, arc or ellipse"));
     action.setDefaultShortcut(new QKeySequence("l,n"));
-    action.setDefaultCommands(["lineorthogonaltangent", "orthotangent", "ln"]);
+    action.setDefaultCommands(["lineorthogonaltangent", "orthotangent"]);
     action.setGroupSortOrder(6500);
     action.setSortOrder(300);
     action.setWidgetNames(["DrawLineMenu", "LineToolBar", "LineToolsPanel", "LineMatrixPanel"]);
