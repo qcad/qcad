3e4eb41105d7e3ba68bceb4637f561832ee1aae7
Andrew Mustun <andrew@qcad.org>
2016-04-07 15:57:50 +0200

    update alpha numerical sort function

--- scripts/library.js
+++ scripts/library.js
@@ -1299,25 +1299,75 @@ Array.prototype.sortNumerical = function() {
     Array.prototype.sort.call(this, function(a,b) { return a - b });
 }
 
+//Array.alphaNumericalSorter = function(a, b) {
+//    var aVal = parseInt(a);
+//    var bVal = parseInt(b);
+//    if (isNaN(aVal) && isNaN(bVal)) {
+//        // alphabetical:
+//        return a.localeCompare(b);
+//    }
+//    else if (isNaN(aVal)) {
+//        // text before numbers:
+//        return -1;
+//    }
+//    else if (isNaN(bVal)) {
+//        // numbers after texts:
+//        return 1;
+//    }
+//    else {
+//        // nummerical:
+//        return aVal - bVal;
+//    }
+//};
+
+/* alphanum.js (C) Brian Huisman
+ * Based on the Alphanum Algorithm by David Koelle
+ * The Alphanum Algorithm is discussed at http://www.DaveKoelle.com
+ *
+ * Distributed under same license as original
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 Array.alphaNumericalSorter = function(a, b) {
-    var aVal = parseInt(a);
-    var bVal = parseInt(b);
-    if (isNaN(aVal) && isNaN(bVal)) {
-        // alphabetical:
-        return a.localeCompare(b);
+    function chunkify(t) {
+        var tz = new Array();
+        var x = 0, y = -1, n = 0, i, j;
+
+        while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+            var m = (i == 46 || (i >=48 && i <= 57));
+            if (m !== n) {
+                tz[++y] = "";
+                n = m;
+            }
+            tz[y] += j;
+        }
+        return tz;
     }
-    else if (isNaN(aVal)) {
-        // text before numbers:
-        return -1;
+
+    var aa = chunkify(a);
+    var bb = chunkify(b);
+
+    for (x = 0; aa[x] && bb[x]; x++) {
+        if (aa[x] !== bb[x]) {
+            var c = Number(aa[x]), d = Number(bb[x]);
+            if (c == aa[x] && d == bb[x]) {
+                return c - d;
+            } else return (aa[x] > bb[x]) ? 1 : -1;
+        }
     }
-    else if (isNaN(bVal)) {
-        // numbers after texts:
-        return 1;
-    }
-    else {
-        // nummerical:
-        return aVal - bVal;
-    }
+    return aa.length - bb.length;
 };
 
 /**
