5e4ffa5a9c9ae3ce5860b680237067295de28005
Andrew Mustun <andrew@qcad.org>
2016-04-28 17:31:03 +0200

    change text to delete layer

--- scripts/Layer/RemoveLayer/RemoveLayerInit.js
+++ scripts/Layer/RemoveLayer/RemoveLayerInit.js
@@ -1,10 +1,10 @@
 function init(basePath) {
-    var action = new RGuiAction(qsTranslate("RemoveLayer", "&Remove Layer"), RMainWindowQt.getMainWindow());
+    var action = new RGuiAction(qsTranslate("RemoveLayer", "&Delete Layer"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/RemoveLayer.js");
     action.setIcon(basePath + "/RemoveLayer.svg");
     action.setDefaultShortcut(new QKeySequence("y,r"));
-    action.setDefaultCommands(["layerremove", "yr"]);
+    action.setDefaultCommands(["layerdelete", "yr"]);
     action.setGroupSortOrder(14300);
     action.setSortOrder(100);
     action.setWidgetNames(["LayerMenu", "LayerToolBar", "LayerToolsPanel", "LayerMatrixPanel"]);
