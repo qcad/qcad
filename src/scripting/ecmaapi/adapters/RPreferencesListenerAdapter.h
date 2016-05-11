5a3540dddada6d4cc6e571de963e6e5c3867a68a
Andrew Mustun <andrew@qcad.org>
2016-05-11 14:44:31 +0200

    update script bindings

---
+++ src/scripting/ecmaapi/adapters/RPreferencesListenerAdapter.h
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2011-2016 by Andrew Mustun. All rights reserved.
+ * 
+ * This file is part of the QCAD project.
+ *
+ * QCAD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QCAD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QCAD.
+ */
+
+#ifndef RPREFERENCESLISTENERADAPTER_H
+#define RPREFERENCESLISTENERADAPTER_H
+
+#include <QMetaType>
+#include <QObject>
+
+#include "RPreferencesListener.h"
+
+class RDocumentInterface;
+
+
+
+/**
+ * \ingroup ecma
+ * \scriptable
+ */
+class RPreferencesListenerAdapter : public QObject, public RPreferencesListener {
+Q_OBJECT
+
+public:
+    virtual ~RPreferencesListenerAdapter() {}
+
+    virtual void updatePreferences(RDocumentInterface* di) {
+        emit preferencesUpdated(di);
+    }
+
+signals:
+    void preferencesUpdated(RDocumentInterface* di);
+};
+
+Q_DECLARE_METATYPE(RPreferencesListenerAdapter*)
+
+#endif
