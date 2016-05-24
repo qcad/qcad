028344056ef46fbfbc02f8ca1dfda2bf0ebe2952
Andrew Mustun <andrew@qcad.org>
2016-05-24 19:29:31 +0200

    update simple API doc

--- scripts/simple.js
+++ scripts/simple.js
@@ -3,69 +3,30 @@
  */
 
 include("scripts/library.js");
+include("scripts/input.js");
 
-PI = Math.PI;
-LN2 = Math.LN2;
-LN10 = Math.LN10;
-LOG2E = Math.LOG2E;
-LOG10E = Math.LOG10E;
-SQRT1_2 = Math.SQRT1_2;
-SQRT2 = Math.SQRT2;
-abs = Math.abs;
-ceil = Math.ceil;
-floor = Math.floor;
-exp = Math.exp;
-log = Math.log;
-max = Math.max;
-min = Math.min;
-pow = Math.pow;
-sqrt = Math.sqrt;
-random = Math.random;
-round = Math.round;
-simpleUseOp = false;
-simpleOp = undefined;
+// internal:
+__simpleUseOp = false;
+__simpleOp = undefined;
 
-function rad2deg(a) { 
-    return a / (2.0 * Math.PI) * 360.0; 
-}
-
-function deg2rad(a) { 
-    return (a / 360.0) * (2.0 * Math.PI); 
-}
-
-function sin(v) { 
-    return Math.sin(deg2rad(v)); 
-}
-
-function cos(v) { 
-    return Math.cos(deg2rad(v)); 
-}
-
-function tan(v) { 
-    return Math.tan(deg2rad(v)); 
-}
-
-function asin(v) { 
-    return rad2deg(Math.asin(v)); 
-}
-
-function acos(v) {
-    return rad2deg(Math.acos(v)); 
-}
-
-function atan(v) { 
-    return rad2deg(Math.atan(v)); 
-}
-
+/**
+ * Returns a pointer to the main application window (RMainWindowQt).
+ */
 function getMainWindow() {
     return RMainWindowQt.getMainWindow();
 }
 
+/**
+ * Returns the current RDocumentInterface or undefined.
+ */
 function getDocumentInterface() {
     var appWin = getMainWindow();
     return appWin.getDocumentInterface();
 }
 
+/**
+ * Returns the current RDocument or undefined.
+ */
 function getDocument() {
     var di = getDocumentInterface();
     if (isNull(di)) {
@@ -74,6 +35,14 @@ function getDocument() {
     return di.getDocument();
 }
 
+/**
+ * Adds a point to the drawing.
+ *
+ * \code
+ * addPoint(x,y)
+ * addPoint(new RVector(x,y))
+ * \endcode
+ */
 function addPoint(p1, p2) {
     if (isNumber(p1)) {
         addPoint(new RVector(p1, p2));
@@ -83,6 +52,12 @@ function addPoint(p1, p2) {
     addShape(new RPoint(p1));
 }
 
+/**
+ * Adds a line to the drawing.
+ *
+ * addLine(x1,y1, x2,y2)
+ * addLine(new RVector(x1,y1), new RVector(x2,y2))
+ */
 function addLine(p1, p2, p3, p4) {
     if (isNumber(p1)) {
         addLine(new RVector(p1, p2), new RVector(p3, p4));
@@ -92,6 +67,12 @@ function addLine(p1, p2, p3, p4) {
     addShape(new RLine(p1, p2));
 }
 
+/**
+ * Adds an arc to the drawing.
+ *
+ * addArc(cx,cy, radius, startAngle, endAngle, reversed)
+ * addArc(new RVector(cx,cy), radius, startAngle, endAngle, reversed)
+ */
 function addArc(p1, p2, p3, p4, p5, p6) {
     if (isNumber(p1)) {
         addArc(new RVector(p1, p2), p3, p4, p5, p6);
@@ -100,6 +81,12 @@ function addArc(p1, p2, p3, p4, p5, p6) {
     addShape(new RArc(p1, p2, p3, p4, p5));
 }
 
+/**
+ * Adds a circle to the drawing.
+ *
+ * addCircle(cx,cy, radius)
+ * addCircle(new RVector(cx,cy), radius)
+ */
 function addCircle(p1, p2, p3) {
     if (isNumber(p1)) {
         addCircle(new RVector(p1, p2), p3);
@@ -109,7 +96,13 @@ function addCircle(p1, p2, p3) {
 }
 
 /**
+ * Adds a polyline to the drawing.
+ *
  * \param points Array of RVector or [x,y] tuples.
+ * \param closed True for an implicitely closed polyline.
+ *
+ * addPolyline([[x1,y1],[x2,y2],[x3,y3]], false)
+ * addPolyline([new RVector(x1,y1)],new RVector(x2,y2),new RVector(x3,y3)], false)
  */
 function addPolyline(points, closed) {
     if (isNull(closed)) {
@@ -128,6 +121,23 @@ function addPolyline(points, closed) {
     addShape(pl);
 }
 
+/**
+ * Adds a simple text to the drawing.
+ *
+ * \param text Text string.
+ * \param x X position
+ * \param y Y position
+ * \param height Text height (defaults to 1)
+ * \param angle Text angle (defaults to 0)
+ * \param font Font (defaults to "standard")
+ * \param vAlign Vertical alignment (defaults to RS.VAlignTop)
+ * \param hAlign Horizontal alignment (defaults to RS.HAlignLeft)
+ * \param bold True for bold text (TTF fonts only)
+ * \param italic True for italic text (TTF fonts only)
+ *
+ * addPolyline([[x1,y1],[x2,y2],[x3,y3]], false)
+ * addPolyline([new RVector(x1,y1)],new RVector(x2,y2),new RVector(x3,y3)], false)
+ */
 function addSimpleText(text, x, y, height, angle, font, vAlign, hAlign, bold, italic) {
     if (isNull(height)) height = 1.0;
     if (isNull(angle)) angle = 0.0;
@@ -160,18 +170,24 @@ function addSimpleText(text, x, y, height, angle, font, vAlign, hAlign, bold, it
     addEntity(entity);
 }
 
+/**
+ * Adds the given RShape to the drawing using current layer and attributes.
+ */
 function addShape(shape) {
     var di = getDocumentInterface();
     var entity = shapeToEntity(getDocument(), shape);
     addEntity(entity);
 }
 
+/**
+ * Adds the given REntity to the drawing using layer and attributes as set by the entity.
+ */
 function addEntity(entity) {
-    if (simpleUseOp===true) {
-        if (isNull(simpleOp)) {
-            simpleOp = new RAddObjectsOperation();
+    if (__simpleUseOp===true) {
+        if (isNull(__simpleOp)) {
+            __simpleOp = new RAddObjectsOperation();
         }
-        simpleOp.addObject(entity, false);
+        __simpleOp.addObject(entity, false);
     }
     else {
         var di = getDocumentInterface();
@@ -179,18 +195,32 @@ function addEntity(entity) {
     }
 }
 
+/**
+ * Starts a transaction. This can increase performance when adding multiple entities.
+ * Entities are added in one transaction when endTransaction is called.
+ *
+ * startTransaction();
+ * for (...) {
+ *     addLine(...);
+ * }
+ * endTransaction();
+ */
 function startTransaction() {
-    simpleUseOp = true;
-    if (!isNull(simpleOp)) {
-        simpleOp.destroy();
-        simpleOp = undefined;
+    __simpleUseOp = true;
+    if (!isNull(__simpleOp)) {
+        __simpleOp.destroy();
+        __simpleOp = undefined;
     }
 }
 
+/**
+ * \see startTransaction
+ */
 function endTransaction() {
-    if (!isNull(simpleOp)) {
+    if (!isNull(__simpleOp)) {
         var di = getDocumentInterface();
-        di.applyOperation(simpleOp);
-        simpleOp = undefined;
+        di.applyOperation(__simpleOp);
+        __simpleOp = undefined;
     }
+    __simpleUseOp = false;
 }
