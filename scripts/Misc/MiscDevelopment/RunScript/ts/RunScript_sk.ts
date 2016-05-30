b620c1132866874a333a636e31dbf8001d59974a
Andrew Mustun <andrew@qcad.org>
2016-05-30 17:12:20 +0200

    update translations

---
+++ scripts/Misc/MiscDevelopment/RunScript/ts/RunScript_sk.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk_SK">
+<context>
+    <name>RunScript</name>
+    <message>
+        <location filename="../RunScript.js" line="+60"/>
+        <source>Open Script File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>All Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>ECMAScript Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Run Script</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../RunScriptInit.js" line="+2"/>
+        <source>R&amp;un Script</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Open and run a script file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
