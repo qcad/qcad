cbd380ace36458b10706733535daf025b407f9d8
Andrew Mustun <andrew@qcad.org>
2016-06-08 10:06:45 +0200

    update translations

--- scripts/Misc/MiscDevelopment/EcmaScriptShell/ts/EcmaScriptShell_de.ts
+++ scripts/Misc/MiscDevelopment/EcmaScriptShell/ts/EcmaScriptShell_de.ts
@@ -4,7 +4,12 @@
 <context>
     <name>EcmaScriptShell</name>
     <message>
-        <location filename="../EcmaScriptShell.js" line="+67"/>
+        <location filename="../EcmaScriptShell.js" line="+58"/>
+        <source>Show ECMAScript Shell</source>
+        <translation>ECMAScript Shell anzeigen</translation>
+    </message>
+    <message>
+        <location line="+38"/>
         <source>&amp;Script Shell</source>
         <translation>&amp;Skriptshell</translation>
     </message>
@@ -13,6 +18,21 @@
         <location filename="../EcmaScriptShell.ui" line="+14"/>
         <source>Script Shell</source>
         <translation>Skriptshell</translation>
+    </message>
+    <message>
+        <location filename="../EcmaScriptShellDialog.ui" line="+20"/>
+        <source>ECMAScript Shell</source>
+        <translation>ECMAScript Shell</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;You are about to enable the ECMAScript Shell.&lt;/p&gt;&lt;p&gt;Through the ECMAScript Shell you can &lt;span style=&quot; color:#ff0000;&quot;&gt;modify your drawings&lt;/span&gt; but also &lt;span style=&quot; color:#ff0000;&quot;&gt;delete files&lt;/span&gt; or &lt;span style=&quot; color:#ff0000;&quot;&gt;crash the application&lt;/span&gt; which can lead to &lt;span style=&quot; color:#ff0000;&quot;&gt;data loss&lt;/span&gt;.&lt;/p&gt;&lt;p&gt;For inexperienced programmers, it is recommended to limit the use of the ECMAScript shell to the &lt;a href=&quot;http://www.qcad.org/doc/qcad/latest/developer/group__ecma__simple.html&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;QCAD Simple API&lt;/span&gt;&lt;/a&gt; which is considered to be &apos;safe&apos; to use.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Anzeigen der ECMAScript Shell.&lt;/p&gt;&lt;p&gt;Mit Hilfe der ECMAScript Shell können Sie &lt;span style=&quot;color:#ff0000;&quot;&gt;Zeichnungen ändern&lt;/span&gt; aber auch &lt;span style=&quot;color:#ff0000;&quot;&gt;Dateien löschen&lt;/span&gt; oder &lt;span style=&quot;color:#ff0000;&quot;&gt;einen Absturz der Anwendung verursachen&lt;/span&gt;, was zu &lt;span style=&quot;color:#ff0000;&quot;&gt;Datenverlust&lt;/span&gt; führen kann.&lt;/p&gt;&lt;p&gt;Für unerfahrene Programmierer, wird die ausschliessliche Verwendung der &lt;a href=&quot;http://www.qcad.org/doc/qcad/latest/developer/group__ecma__simple.html&quot;&gt;QCAD Simple API&lt;/a&gt; empfohlen. Diese wird als &quot;sicher&quot; eingestuft.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Don&apos;t show this dialog again</source>
+        <translation>Diesen Dialog nicht mehr anzeigen</translation>
     </message>
 </context>
 </TS>
--- scripts/Misc/MiscDevelopment/EcmaScriptShell/ts/EcmaScriptShell_ru.ts
+++ scripts/Misc/MiscDevelopment/EcmaScriptShell/ts/EcmaScriptShell_ru.ts
@@ -4,7 +4,12 @@
 <context>
     <name>EcmaScriptShell</name>
     <message>
-        <location filename="../EcmaScriptShell.js" line="+67"/>
+        <location filename="../EcmaScriptShell.js" line="+58"/>
+        <source>Show ECMAScript Shell</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+38"/>
         <source>&amp;Script Shell</source>
         <translation>&amp;Оболочка скрипта</translation>
     </message>
@@ -13,6 +18,21 @@
         <location filename="../EcmaScriptShell.ui" line="+14"/>
         <source>Script Shell</source>
         <translation>Оболочка скрипта</translation>
+    </message>
+    <message>
+        <location filename="../EcmaScriptShellDialog.ui" line="+20"/>
+        <source>ECMAScript Shell</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;You are about to enable the ECMAScript Shell.&lt;/p&gt;&lt;p&gt;Through the ECMAScript Shell you can &lt;span style=&quot; color:#ff0000;&quot;&gt;modify your drawings&lt;/span&gt; but also &lt;span style=&quot; color:#ff0000;&quot;&gt;delete files&lt;/span&gt; or &lt;span style=&quot; color:#ff0000;&quot;&gt;crash the application&lt;/span&gt; which can lead to &lt;span style=&quot; color:#ff0000;&quot;&gt;data loss&lt;/span&gt;.&lt;/p&gt;&lt;p&gt;For inexperienced programmers, it is recommended to limit the use of the ECMAScript shell to the &lt;a href=&quot;http://www.qcad.org/doc/qcad/latest/developer/group__ecma__simple.html&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;QCAD Simple API&lt;/span&gt;&lt;/a&gt; which is considered to be &apos;safe&apos; to use.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Don&apos;t show this dialog again</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
