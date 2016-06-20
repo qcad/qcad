50c81d2711991ba8a96a7e2de45e8764be79bc81
Andrew Mustun <andrew@qcad.org>
2016-06-20 16:48:03 +0200

    update translations

--- scripts/Misc/MiscDevelopment/RunScript/ts/RunScript_sk.ts
+++ scripts/Misc/MiscDevelopment/RunScript/ts/RunScript_sk.ts
@@ -41,7 +41,7 @@
     <message>
         <location line="+15"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;You are about to run the following script(s):&lt;/p&gt;&lt;p&gt;%1&lt;/p&gt;&lt;p&gt;This script could potentially modify your drawings, modify or delete your files or access your network connection.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; color:#ff0000;&quot;&gt;It is strongly recommended to only run scripts from trusted sources.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Chystáte sa spustiť nasledujúci skript(y):&lt;/p&gt;&lt;p&gt;%1&lt;/p&gt;&lt;p&gt;Tento skript by mohol potenciálne meniť Vaše výkresy, modifikovať alebo mazať súbory alebo pripojenie na prístup k sieti.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-weight:600; color:#FF0000;&quot;&gt;Dôrazne sa odporúča spúšťať skripty iba z dôveryhodných zdrojov.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location line="+10"/>
