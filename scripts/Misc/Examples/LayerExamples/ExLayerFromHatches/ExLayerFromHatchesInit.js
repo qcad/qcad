25a41c2d34e695a056925bd67ef89ce0bbf5ac0f
Andrew Mustun <andrew@qcad.org>
2016-06-20 15:02:56 +0200

    remove reference to non existing icon for example tool

--- scripts/Misc/Examples/LayerExamples/ExLayerFromHatches/ExLayerFromHatchesInit.js
+++ scripts/Misc/Examples/LayerExamples/ExLayerFromHatches/ExLayerFromHatchesInit.js
@@ -2,7 +2,6 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("ExLayerFromHatches", "&Create New Layer from Hatches"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/ExLayerFromHatches.js");
-    action.setIcon(basePath + "/ExLayerFromHatches.svg");
     action.setGroupSortOrder(78100);
     action.setSortOrder(500);
     action.setWidgetNames(["LayerExamplesMenu"]);
