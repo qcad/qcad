6abb3b5f31858f56ddc76deaafd5ef4f53d0d1da
Andrew Mustun <andrew@qcad.org>
2016-09-16 14:29:08 +0200

    minor update

--- scripts/Widgets/LayerList/LayerList.js
+++ scripts/Widgets/LayerList/LayerList.js
@@ -103,7 +103,7 @@ RLayerListQt.prototype.updateLayers = function(documentInterface) {
     layer = doc.queryCurrentLayer();
     if (!layer.isNull()) {
         var list = this.findItems(layer.getName(), Qt.MatchExactly);
-        if (list.length != 0) {
+        if (list.length !== 0) {
             this.blockSignals(true);
             this.setCurrentItem(list[0]);
             this.blockSignals(false);
