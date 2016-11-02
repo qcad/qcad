ada6408c10e1aeeea30998423e9874b0d1d68f4c
Andrew Mustun <andrew@qcad.org>
2016-11-02 10:28:56 +0100

    fix saving of floating dock widget state under os x when locale is not English

--- scripts/File/Quit/QuitInit.js
+++ scripts/File/Quit/QuitInit.js
@@ -1,10 +1,18 @@
 function init(basePath) {
-    var action = new RGuiAction(qsTranslate("Quit", "&Quit"), RMainWindowQt.getMainWindow());
+    var title;
+    if (RS.getSystemId()==="osx") {
+        // don't translate quit menu under OS X:
+        title = "&Quit";
+    }
+    else {
+        title = qsTranslate("Quit", "&Quit");
+    }
+
+    var action = new RGuiAction(title, RMainWindowQt.getMainWindow());
     if (RS.getSystemId() === "osx") {
         action.disableIcon();
     }
     action.setRequiresDocument(false);
-    //action.setScriptFile(basePath + "/Quit.js");
     action.setDefaultShortcut(new QKeySequence(QKeySequence.Quit));
     action.setDefaultCommands(["quit", "exit"]);
     action.checkable = false;
