3bbb4a17c32541df24750a43f83bec671e2da262
Andrew Mustun <andrew@qcad.org>
2016-12-12 08:49:48 +0100

    set version to 3.16.0.0

--- src/core/RVersion.h
+++ src/core/RVersion.h
@@ -25,11 +25,11 @@
  * x.x.x   for releases
  * x.x.x.x for snapshots only
  */
-#define R_QCAD_VERSION_STRING "3.15.6.3"
+#define R_QCAD_VERSION_STRING "3.16.0"
 
 #define R_QCAD_VERSION_MAJOR    3
-#define R_QCAD_VERSION_MINOR    15
-#define R_QCAD_VERSION_REV      6
-#define R_QCAD_VERSION_BUILD    3
+#define R_QCAD_VERSION_MINOR    16
+#define R_QCAD_VERSION_REV      0
+#define R_QCAD_VERSION_BUILD    0
 
 #endif
