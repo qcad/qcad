18d69c7a340d939cc0c8c2d252fb8f4ac7a34e35
Andrew Mustun <andrew@qcad.org>
2016-12-21 18:54:40 +0100

    add translations for ConvertUnit

---
+++ scripts/Edit/ConvertUnit/ts/ConvertUnit_cs.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>ConvertUnit</name>
+    <message>
+        <location filename="../ConvertUnit.js" line="81"/>
+        <source>Nothing to be done.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="84"/>
+        <source>RS.None selected - Nothing to be done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="136"/>
+        <source>Converted drawing from %1 to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="143"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="144"/>
+        <source>Inch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="145"/>
+        <source>Foot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="146"/>
+        <source>Mile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="147"/>
+        <source>Millimeter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="148"/>
+        <source>Centimeter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="149"/>
+        <source>Meter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="150"/>
+        <source>Kilometer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="151"/>
+        <source>Microinch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="152"/>
+        <source>Mil</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="153"/>
+        <source>Yard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="154"/>
+        <source>Angstrom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="155"/>
+        <source>Nanometer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="156"/>
+        <source>Micron</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="157"/>
+        <source>Decimeter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="158"/>
+        <source>Decameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="159"/>
+        <source>Hectometer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="160"/>
+        <source>Gigameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="161"/>
+        <source>Astro</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="162"/>
+        <source>Lightyear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnit.js" line="163"/>
+        <source>Parsec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnitInit.js" line="2"/>
+        <source>Convert Drawing Units</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ConvertUnitInit.js" line="6"/>
+        <source>Change drawing units</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
