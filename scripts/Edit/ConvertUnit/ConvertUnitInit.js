a3d9ea3d69410f18bfeaa1473afdfd6330bcaaf2
Andrew Mustun <andrew@qcad.org>
2016-12-22 11:57:21 +0100

    update tool titles

--- scripts/Edit/ConvertUnit/ConvertUnitInit.js
+++ scripts/Edit/ConvertUnit/ConvertUnitInit.js
@@ -1,9 +1,9 @@
 function init(basePath) {
-    var action = new RGuiAction(qsTranslate("ConvertUnit", "Convert Drawing Units"), RMainWindowQt.getMainWindow());
+    var action = new RGuiAction(qsTranslate("ConvertUnit", "Convert Drawing &Unit"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/ConvertUnit.js");
     action.setIcon(basePath + "/ConvertUnit.svg");
-    action.setStatusTip(qsTranslate("ConvertUnit", "Change drawing units"));
+    action.setStatusTip(qsTranslate("ConvertUnit", "Convert drawing to a different drawing unit"));
     action.setDefaultShortcut(new QKeySequence("c,u"));
     action.setDefaultCommands(["convertunit", "cu"]);
     action.setGroupSortOrder(2650);
