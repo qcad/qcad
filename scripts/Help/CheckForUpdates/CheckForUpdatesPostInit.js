8b13cc6da5a02c5f4f6ea43618b30b3a98f1e21f
Andrew Mustun <andrew@qcad.org>
2017-02-28 12:45:13 +0100

    disable update check if it causes crash (QTBUG-56747)

--- scripts/Help/CheckForUpdates/CheckForUpdatesPostInit.js
+++ scripts/Help/CheckForUpdates/CheckForUpdatesPostInit.js
@@ -9,9 +9,18 @@ function postInit() {
     if (RSettings.getBoolValue("CheckForUpdates/AutoCheckUpdates", false)===true &&
         appWin.property("CheckForUpdates/Handled")!==true) {
 
+        // disable check in case check would crash app (macOS, Qt 5.8.0, QTBUG-56747):
+        RSettings.setValue("CheckForUpdates/AutoCheckUpdates", false);
+        RSettings.getQSettings().sync();
+
         qDebug("checking for updates...");
         include("CheckForUpdates.js");
         var html = download(CheckForUpdates.getUrl(), 3000);
+
+        // re-enable check if no crash occurred:
+        RSettings.setValue("CheckForUpdates/AutoCheckUpdates", true);
+        RSettings.getQSettings().sync();
+
         if (html.indexOf("<title>Up To Date</title>")!==-1) {
             qDebug("no updates available");
         }
