b14368b64a0121713547d1c699c3a45ea6058f1f
Andrew Mustun <andrew@qcad.org>
2017-06-22 18:04:27 +0200

    add File > Close shortcut under Linux when using Qt 5

--- scripts/File/CloseFile/CloseFileInit.js
+++ scripts/File/CloseFile/CloseFileInit.js
@@ -3,7 +3,7 @@ function init(basePath) {
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/CloseFile.js");
     action.setIcon(basePath + "/CloseFile.svg");
-    if (RS.getSystemId()!=="linux") {
+    if (RS.getSystemId()!=="linux" || RSettings.isQt(5)) {
         action.setDefaultShortcut(new QKeySequence(QKeySequence.Close));
     }
     action.setDefaultCommands(["close"]);
