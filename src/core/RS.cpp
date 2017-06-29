470cda68956087e8061791d4814a206f0f2eb8b2
Andrew Mustun <andrew@qcad.org>
2017-06-29 09:12:17 +0200

    fix FS#1609 - Edit > Drawing Preferences: dimension size scale cannot be changed to 1

--- src/core/RS.cpp
+++ src/core/RS.cpp
@@ -29,42 +29,74 @@ const double RS::AngleTolerance = 1.0e-9;
 
 bool RS::compare(const QVariant& v1, const QVariant& v2) {
     // 20120609: tolerance when comparing doubles (property editor)
-    // 20140513: handle basic types since Qt 5 thinks it can convert double, bool, int to line type
-    if (v1.type()==QVariant::Double && v2.type()==QVariant::Double) {
-        return fabs(v1.toDouble() - v2.toDouble()) < RS::PointTolerance;
-    }
-    if (v1.type()==QVariant::Bool && v2.type()==QVariant::Bool) {
-        return v1.toBool()==v2.toBool();
-    }
-    if (v1.type()==QVariant::Int && v2.type()==QVariant::Int) {
-        return v1.toInt()==v2.toInt();
-    }
-    if (v1.type()==QVariant::UInt && v2.type()==QVariant::UInt) {
-        return v1.toUInt()==v2.toUInt();
-    }
-    if (v1.type()==QVariant::LongLong && v2.type()==QVariant::LongLong) {
-        return v1.toLongLong()==v2.toLongLong();
-    }
-    if (v1.type()==QVariant::ULongLong && v2.type()==QVariant::ULongLong) {
-        return v1.toULongLong()==v2.toULongLong();
-    }
-    if (v1.type()==QVariant::String && v2.type()==QVariant::String) {
-        return v1.toString()==v2.toString();
-    }
+    // 20140513: handle basic types since Qt 5 converts double, bool, int to line type
+    switch (v1.type()) {
+    case QVariant::Double:
+        if (v2.type()==QVariant::Double) {
+            return fabs(v1.toDouble() - v2.toDouble()) < RS::PointTolerance;
+        }
+        else if (v2.type()==QVariant::Int) {
+            return fabs(v1.toDouble() - v2.toInt()) < RS::PointTolerance;
+        }
+        break;
 
-    if (v1.canConvert<RColor> () && v2.canConvert<RColor> ()) {
-        return v1.value<RColor> () == v2.value<RColor> ();
+    case QVariant::Int:
+        if (v2.type()==QVariant::Int) {
+            return v1.toInt()==v2.toInt();
+        }
+        else if (v2.type()==QVariant::Double) {
+            return fabs(v1.toInt() - v2.toDouble()) < RS::PointTolerance;
+        }
+        break;
+
+    case QVariant::Bool:
+        if (v2.type()==QVariant::Bool) {
+            return v1.toBool()==v2.toBool();
+        }
+        break;
+
+    case QVariant::UInt:
+        if (v2.type()==QVariant::UInt) {
+            return v1.toUInt()==v2.toUInt();
+        }
+        break;
+
+    case QVariant::LongLong:
+        if (v2.type()==QVariant::LongLong) {
+            return v1.toLongLong()==v2.toLongLong();
+        }
+        break;
+
+    case QVariant::ULongLong:
+        if (v2.type()==QVariant::ULongLong) {
+            return v1.toULongLong()==v2.toULongLong();
+        }
+        break;
+
+    case QVariant::String:
+        if (v2.type()==QVariant::String) {
+            return v1.toString()==v2.toString();
+        }
+        break;
+
+    default:
+        if (v1.canConvert<RColor> () && v2.canConvert<RColor> ()) {
+            return v1.value<RColor> () == v2.value<RColor> ();
+        }
+        else if (v1.canConvert<RLinetype> () && v2.canConvert<RLinetype> ()) {
+            return v1.value<RLinetype> () == v2.value<RLinetype> ();
+        }
+        else if (v1.canConvert<RLineweight::Lineweight> () && v2.canConvert<RLineweight::Lineweight> ()) {
+            return v1.value<RLineweight::Lineweight> () == v2.value<RLineweight::Lineweight> ();
+        }
+        else if (v1.canConvert<QList<RVector> > () && v2.canConvert<QList<RVector> > ()) {
+            return v1.value<QList<RVector> > () == v2.value<QList<RVector> > ();
+        }
+        else {
+            return v1 == v2;
+        }
+        break;
     }
-    if (v1.canConvert<RLinetype> () && v2.canConvert<RLinetype> ()) {
-        return v1.value<RLinetype> () == v2.value<RLinetype> ();
-    }
-    if (v1.canConvert<RLineweight::Lineweight> () && v2.canConvert<RLineweight::Lineweight> ()) {
-        return v1.value<RLineweight::Lineweight> () == v2.value<RLineweight::Lineweight> ();
-    }
-    if (v1.canConvert<QList<RVector> > () && v2.canConvert<QList<RVector> > ()) {
-        return v1.value<QList<RVector> > () == v2.value<QList<RVector> > ();
-    }
-    return v1 == v2;
 }
 
 bool RS::compare(const QPair<QVariant, RPropertyAttributes>& p1, const QPair<
