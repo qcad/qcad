894407e62d32c805f7d271a85155edc4e4a33e1e
Andrew Mustun <andrew@qcad.org>
2017-09-28 14:52:55 +0100

    remove example script to convert white layers to black

--- scripts/Misc/Examples/LayerExamples/ExLayerFromHatches/ExLayerFromHatchesInit.js
+++
@@ -1,8 +0,0 @@
-function init(basePath) {
-    var action = new RGuiAction(qsTranslate("ExLayerFromHatches", "&Create New Layer from Hatches"), RMainWindowQt.getMainWindow());
-    action.setRequiresDocument(true);
-    action.setScriptFile(basePath + "/ExLayerFromHatches.js");
-    action.setGroupSortOrder(78100);
-    action.setSortOrder(500);
-    action.setWidgetNames(["LayerExamplesMenu"]);
-}
