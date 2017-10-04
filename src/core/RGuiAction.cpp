248b8e7b81de7b49ea581b87791e94efa590dc12
Andrew Mustun <andrew@qcad.org>
2017-10-04 10:31:30 +0100

    fix icon resolution
    if tool bar icon size is larger than cad tool bar icon size

--- src/core/RGuiAction.cpp
+++ src/core/RGuiAction.cpp
@@ -231,6 +231,7 @@ void RGuiAction::setIcon(const QString& iconFile) {
             !QCoreApplication::arguments().contains("-max-icon-res")) {
 
             int iconSize = RSettings::getIntValue("CadToolBar/IconSize", 32);
+            iconSize = qMax(iconSize, RSettings::getIntValue("ToolBar/IconSize", 32));
             iconSize = qMax(iconSize, RSettings::getIntValue("CadToolMatrix/IconSize", 24));
 
             // retina icons:
