5acf63ec91add9a0ec81fe3a7f53bb6db40e3905
Andrew Mustun <andrew@qcad.org>
2017-10-06 11:39:38 +0100

    add example script

---
+++ examples/scripts/createdrawing.js
@@ -0,0 +1,16 @@
+// run with 
+// qcad -autostart examples/scripts/createdrawing.js
+
+include("scripts/simple.js");
+
+// create a document:
+var doc = createOffScreenDocument();
+
+// add something to the document:
+startTransaction(doc);
+addLine(0,0, 100,100);
+endTransaction();
+
+// export the document to a DXF file:
+var di = new RDocumentInterface(doc);
+di.exportFile("qcad_output.dxf");
