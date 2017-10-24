565d94550aa5286a8f357dd86602e339f7d4ab75
Andrew Mustun <andrew@qcad.org>
2017-10-24 17:12:18 +0100

    add rotation parameter

--- scripts/Draw/DrawBasedOnRectanglePP.js
+++ scripts/Draw/DrawBasedOnRectanglePP.js
@@ -34,6 +34,7 @@ function DrawBasedOnRectanglePP(guiAction) {
 
     this.corner1 = undefined;
     this.corner2 = undefined;
+    this.rotation = 0.0;
     this.previewRectangle = false;
 }
 
@@ -128,12 +129,26 @@ DrawBasedOnRectanglePP.prototype.getCorners = function() {
         return [];
     }
 
-    return [
-        new RVector(this.corner1.x, this.corner1.y),
-        new RVector(this.corner2.x, this.corner1.y),
-        new RVector(this.corner2.x, this.corner2.y),
-        new RVector(this.corner1.x, this.corner2.y)
-    ];
+    if (RMath.fuzzyCompare(this.rotation, 0.0)) {
+        return [
+            new RVector(this.corner1.x, this.corner1.y),
+            new RVector(this.corner2.x, this.corner1.y),
+            new RVector(this.corner2.x, this.corner2.y),
+            new RVector(this.corner1.x, this.corner2.y)
+        ];
+    }
+    else {
+        var l1 = new RLine(this.corner1, this.rotation, 1.0);
+        var l2 = new RLine(this.corner2, this.rotation + Math.PI/2, 1.0);
+        var ips = RShape.getIntersectionPointsLL(l1, l2, false);
+        if (ips.length!==1) {
+            return [];
+        }
+        var c3 = ips[0];
+        var v = this.corner1.operator_subtract(c3);
+        var c4 = this.corner2.operator_add(v);
+        return [ this.corner1, c3, this.corner2, c4 ];
+    }
 };
 
 DrawBasedOnRectanglePP.prototype.getCenter = function() {
@@ -141,11 +156,23 @@ DrawBasedOnRectanglePP.prototype.getCenter = function() {
 };
 
 DrawBasedOnRectanglePP.prototype.getWidth = function() {
-    return Math.abs(this.corner1.x-this.corner2.x);
+    if (RMath.fuzzyCompare(this.rotation, 0.0)) {
+        return Math.abs(this.corner1.x-this.corner2.x);
+    }
+    else {
+        var c = this.getCorners();
+        return c[0].getDistanceTo(c[1]);
+    }
 };
 
 DrawBasedOnRectanglePP.prototype.getHeight = function() {
-    return Math.abs(this.corner1.y-this.corner2.y);
+    if (RMath.fuzzyCompare(this.rotation, 0.0)) {
+        return Math.abs(this.corner1.y-this.corner2.y);
+    }
+    else {
+        var c = this.getCorners();
+        return c[1].getDistanceTo(c[2]);
+    }
 };
 
 DrawBasedOnRectanglePP.prototype.getAuxPreview = function() {
