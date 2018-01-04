2ba2c0801807f564d375f8d567f9d0e798ad3b00
Andrew Mustun <andrew@qcad.org>
2018-01-04 20:59:03 +0000

    esc in spinbox returns focus to graphics view

--- scripts/Edit/Esc/Esc.js
+++ scripts/Edit/Esc/Esc.js
@@ -35,7 +35,7 @@ Esc.prototype.beginEvent = function() {
 
     // focus in RMathLineEdit (e.g. in options toolbar)
     // return focus to graphics view:
-    if (isOfType(w, RMathLineEdit)) {
+    if (isOfType(w, RMathLineEdit) || isOfType(w, QSpinBox)) {
         if (w.ignoreEscape===true) {
             // ignore escape and use standard action for escape
             // this is for some actions to make sure esc triggers step back
