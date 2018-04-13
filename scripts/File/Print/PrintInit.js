47241e93bea9d8af5f654eb048b9e8e3764ec674
Andrew Mustun <andrew@qcad.org>
2018-04-13 10:12:20 +0100

    fix printing shortcut

--- scripts/File/Print/PrintInit.js
+++ scripts/File/Print/PrintInit.js
@@ -1,6 +1,7 @@
 function init(basePath) {
     var action = new RGuiAction(qsTranslate("Print", "&Print"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
+    action.setForceGlobal(true);
     action.setScriptFile(basePath + "/Print.js");
     action.setIcon(basePath + "/Print.svg");
     action.setDefaultShortcut(new QKeySequence(Qt.ControlModifier + Qt.Key_P));
