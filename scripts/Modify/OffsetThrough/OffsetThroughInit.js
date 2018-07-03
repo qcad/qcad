06bae1c8a23575e5c7be173f5638947fbd4468ad
Andrew Mustun <andrew@qcad.org>
2018-07-03 22:23:23 +0100

    fix command conflict

--- scripts/Modify/OffsetThrough/OffsetThroughInit.js
+++ scripts/Modify/OffsetThrough/OffsetThroughInit.js
@@ -5,7 +5,7 @@ function init(basePath) {
     action.setIcon(basePath + "/OffsetThrough.svg");
     action.setStatusTip(qsTranslate("OffsetThrough", "Offset line, arc, circle or ellipse through point"));
     action.setDefaultShortcut(new QKeySequence("o,h"));
-    action.setDefaultCommands(["offsetthrough", "offsetthrough", "oh"]);
+    action.setDefaultCommands(["offsetthrough", "oh"]);
     action.setGroupSortOrder(13150);
     action.setSortOrder(200);
     action.setWidgetNames(["ModifyMenu", "ModifyToolBar", "ModifyToolsPanel", "ModifyMatrixPanel"]);
