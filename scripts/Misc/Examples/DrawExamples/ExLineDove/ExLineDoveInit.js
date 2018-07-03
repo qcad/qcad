13bedde2b1f83d373bc1d5814b0af701d64d3327
Andrew Mustun <andrew@qcad.org>
2018-07-03 22:19:00 +0100

    fix command conflict

--- scripts/Misc/Examples/DrawExamples/ExLineDove/ExLineDoveInit.js
+++ scripts/Misc/Examples/DrawExamples/ExLineDove/ExLineDoveInit.js
@@ -4,7 +4,7 @@ function init(basePath) {
     action.setScriptFile(basePath + "/ExLineDove.js");
     action.setIcon(basePath + "/ExLineDove.svg");
     action.setStatusTip(qsTranslate("ExLineDove", "Draw a dovetail joint"));
-    action.setDefaultCommands(["dovetail"]);
+    action.setDefaultCommands(["exdovetail"]);
     action.setGroupSortOrder(73100);
     action.setSortOrder(220);
     action.setWidgetNames(["DrawExamplesMenu"]);
