0fa8f80b6fdb8bdad13f67a3695f91c324506ca7
Andrew Mustun <andrew@qcad.org>
2018-09-06 08:55:38 +0100

    add user manual, translation files

---
+++ scripts/View/ToolMatrixFocus/ts/ToolMatrixFocus_en.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>CommandLineFocus</name>
+    <message>
+        <source>&amp;Focus on Command Line</source>
+        <translation type="vanished">&amp;Focus on Command Line</translation>
+    </message>
+</context>
+<context>
+    <name>ToolMatrixFocus</name>
+    <message>
+        <location filename="../ToolMatrixFocusInit.js" line="+2"/>
+        <source>&amp;Focus on Tool Matrix</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ scripts/View/ToolMatrixFocus/ts/ToolMatrixFocus_nl.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>CommandLineFocus</name>
+    <message>
+        <source>&amp;Focus on Command Line</source>
+        <translation type="vanished">&amp;Focus op commando regel</translation>
+    </message>
+</context>
+<context>
+    <name>ToolMatrixFocus</name>
+    <message>
+        <location filename="../ToolMatrixFocusInit.js" line="+2"/>
+        <source>&amp;Focus on Tool Matrix</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
