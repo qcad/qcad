d06cd26195dc60a01c5fb0f66aab2dec9b525efc
Andrew Mustun <andrew@qcad.org>
2018-11-14 11:23:20 +0000

    make view > toolbars and widgets > widgets / toolbars translatable

--- scripts/View/ViewToolBars/ViewToolBarsPostInit.js
+++ scripts/View/ViewToolBars/ViewToolBarsPostInit.js
@@ -4,6 +4,15 @@ function postInit(basePath) {
     var subMenu = mainWindow.createPopupMenu();
     if (!isNull(subMenu)) {
         subMenu.title = qsTranslate("ViewToolBars", "&Toolbars and Widgets");
+        var actions = subMenu.actions();
+        for (var i=0; i<actions.length; i++) {
+            if (actions[i].text==="Widgets") {
+                actions[i].text = qsTranslate("ViewToolBars", "Widgets");
+            }
+            if (actions[i].text==="Toolbars") {
+                actions[i].text = qsTranslate("ViewToolBars", "Toolbars");
+            }
+        }
         menu.addSeparator();
         menu.addMenu(subMenu);
     }
