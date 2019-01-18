be32635db899b54c1dfeca400c57d2fc58cf3d47
Weblate Admin <info@qcad.org>
2019-01-18 15:08:02 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (2358 of 2358 strings)
    
    Translation: QCAD/scripts
    Translate-URL: http://translate.qcad.org/projects/qcad/scripts/nl/

--- ts/scripts_nl.ts
+++ ts/scripts_nl.ts
@@ -4127,7 +4127,7 @@ is al in de lijst.</translation>
     <message>
         <location filename="../scripts/Misc/Examples/ExMinimal/ExMinimal.js" line="+12"/>
         <source>Hello World.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hallo wereld.</translation>
     </message>
     <message>
         <location line="+6"/>
