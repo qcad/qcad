b2eaa203a3a738ee87de0d25f58fa9c847ea41bf
Andrew Mustun <andrew@qcad.org>
2019-01-29 14:17:07 +0000

    use camelCase consistenly for parameters

--- scripts/File/BitmapExport/BitmapExportWorker.js
+++ scripts/File/BitmapExport/BitmapExportWorker.js
@@ -24,19 +24,21 @@
  * \param fileName File name for exported bitmap. Extension determines format.
  * \param properties Various properties:
  *
- *  properties["width"]: width of bitmap in pixels (ignored if resolution is present)
- *  properties["height"]: height of bitmap in pixels (ignored if resolution is present)
- *  properties["resolution"]: resolution in pixels / drawing unit
- *  properties["margin"]: margin at borders in pixels
- *  properties["backgroundColor"]: background color (RColor)
- *  properties["origin"]: true: export origin point as red cross
- *  properties["antialiasing"]: true: use antialiasing
- *  properties["quality"]: Export quality (0..100), JPEG only
- *  properties["monochrome"]: true: Export as black / white
- *  properties["grayscale"]: true: Export as grayscale
- *  properties["window"]: RBox: window to export in drawing coordinates
- *  properties["entityids"]: Array: zoom to bounding box of given entities
- *  properties["initView"]: Callback to initialize view
+ *  - properties["width"]: width of bitmap in pixels (ignored if resolution is present)
+ *  - properties["height"]: height of bitmap in pixels (ignored if resolution is present)
+ *  - properties["resolution"]: resolution in pixels / drawing unit
+ *  - properties["margin"]: margin at borders in pixels
+ *  - properties["noWeightMargin"]: true to disable margin for lineweight, defaults to false
+ *  - properties["backgroundColor"]: background color (RColor)
+ *  - properties["origin"]: true: export origin point as red cross
+ *  - properties["antialiasing"]: true: use antialiasing
+ *  - properties["quality"]: Export quality (0..100), JPEG only
+ *  - properties["monochrome"]: true: Export as black / white
+ *  - properties["grayscale"]: true: Export as grayscale
+ *  - properties["window"]: RBox: window to export in drawing coordinates
+ *  - properties["entityIds"]: Array: zoom to bounding box of given entities
+ *  - properties["initView"]: Callback to initialize view
+ *  - properties["zoomAll"]: Auto zoom to all entities, including those on invisible layers
  *
  * \param view Optional graphics view to use.
  */
@@ -74,18 +76,18 @@ function exportBitmap(doc, scene, fileName, properties, view) {
         properties["margin"] = 20;
     }
 
-    if (typeof(properties["noweightmargin"])==="undefined") {
-        properties["noweightmargin"] = false;
+    if (typeof(properties["noWeightMargin"])==="undefined") {
+        properties["noWeightMargin"] = false;
     }
 
-    if (typeof(properties["zoomall"])==="undefined") {
-        properties["zoomall"] = false;
+    if (typeof(properties["zoomAll"])==="undefined") {
+        properties["zoomAll"] = false;
     }
 
     if (properties["resolution"]) {
         var bb;
-        if (typeof(properties["entityids"])!=="undefined") {
-            bb = doc.getEntitiesBox(properties["entityids"]);
+        if (typeof(properties["entityIds"])!=="undefined") {
+            bb = doc.getEntitiesBox(properties["entityIds"]);
         }
         else {
             bb = doc.getBoundingBox(true, true);
@@ -112,15 +114,15 @@ function exportBitmap(doc, scene, fileName, properties, view) {
     if (properties["window"]) {
         view.zoomTo(properties["window"], properties["margin"]);
     }
-    else if (properties["zoomall"]) {
+    else if (properties["zoomAll"]) {
         var bbz = doc.getBoundingBox(false, true);
         view.zoomTo(bbz, properties["margin"]);
     }
-    else if (typeof(properties["entityids"])!=="undefined") {
-        view.zoomToEntities(properties["entityids"], properties["margin"]);
+    else if (typeof(properties["entityIds"])!=="undefined") {
+        view.zoomToEntities(properties["entityIds"], properties["margin"]);
     }
     else {
-        view.autoZoom(properties["margin"], true, properties["noweightmargin"]);
+        view.autoZoom(properties["margin"], true, properties["noWeightMargin"]);
     }
 
     // make sure we use the desired resolution:
