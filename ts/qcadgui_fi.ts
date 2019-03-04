d172b74ed990e5ef355e84b905406b8b5917f0ae
Jarmo Hynninen <jarmo_hynninen@hotmail.com>
2019-03-04 19:38:42 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: QCAD/qcadgui
    Translate-URL: http://translate.qcad.org/projects/qcad/qcadgui/fi/

--- ts/qcadgui_fi.ts
+++ ts/qcadgui_fi.ts
@@ -14,47 +14,47 @@
     <message>
         <location filename="../src/gui/REventHandler.cpp" line="+195"/>
         <source>Grid</source>
-        <translation type="unfinished"></translation>
+        <translation>Ruudukko</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>End</source>
-        <translation type="unfinished"></translation>
+        <translation>Loppu</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>On Entity</source>
-        <translation type="unfinished"></translation>
+        <translation>Kokonaisuuteen</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Perpendicular</source>
-        <translation type="unfinished"></translation>
+        <translation>Kohtisuora</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Tangential</source>
-        <translation type="unfinished"></translation>
+        <translation>Tangentiaalinen</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Center</source>
-        <translation type="unfinished"></translation>
+        <translation>Keskusta</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Middle</source>
-        <translation type="unfinished"></translation>
+        <translation>Keskimmäinen</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Intersection</source>
-        <translation type="unfinished"></translation>
+        <translation>Leikkauspiste</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Reference</source>
-        <translation type="unfinished"></translation>
+        <translation>Viite</translation>
     </message>
 </context>
 <context>
@@ -62,7 +62,7 @@
     <message>
         <location filename="../src/gui/RMathLineEdit.cpp" line="+161"/>
         <source>Invalid</source>
-        <translation type="unfinished"></translation>
+        <translation>Virheellinen</translation>
     </message>
 </context>
 </TS>
