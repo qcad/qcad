6003fc80199090ed077a2daa99edfbf897d60c7a
Andrew Mustun <andrew@qcad.org>
2019-05-28 21:32:45 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (2541 of 2541 strings)
    
    Translation: QCAD/scripts
    Translate-URL: http://translate.qcad.org/projects/qcad/scripts/it/

--- ts/scripts_it.ts
+++ ts/scripts_it.ts
@@ -5576,7 +5576,7 @@ Seleziona Singolo Oggetto</translatorcomment>
     <message>
         <location line="+10"/>
         <source>Solid grid lines</source>
-        <translation type="unfinished">linee griglia continue</translation>
+        <translation>Linee griglia continue</translation>
     </message>
     <message>
         <location line="+7"/>
@@ -12819,16 +12819,16 @@ Conversione non riuscita per il livello &quot;%1&quot;</translatorcomment>
         <location filename="../scripts/Snap/Snap.js" line="+57"/>
         <source>Snap Tools</source>
         <translatorcomment>--google--</translatorcomment>
-        <translation type="unfinished">Impostazioni calamita</translation>
+        <translation>Strumenti scatto</translation>
     </message>
     <message>
         <source>Show snap tools</source>
-        <translation type="unfinished">Mostra impostazioni calamita</translation>
+        <translation>Mostra strumenti scatto</translation>
     </message>
     <message>
         <location line="+26"/>
         <source>&amp;Snap</source>
-        <translation type="unfinished">&amp;Calamita</translation>
+        <translation>&amp;Scatto</translation>
     </message>
 </context>
 <context>
@@ -12841,12 +12841,12 @@ Conversione non riuscita per il livello &quot;%1&quot;</translatorcomment>
     <message>
         <location filename="../scripts/Snap/SnapAuto/SnapAuto.js" line="+34"/>
         <source>Snap</source>
-        <translation type="unfinished">Calamita</translation>
+        <translation>Scatto</translation>
     </message>
     <message>
         <location line="+0"/>
         <source>Auto Snap</source>
-        <translation type="unfinished">Auto selezione</translation>
+        <translation>Scatto automatico</translation>
     </message>
 </context>
 <context>
@@ -14447,7 +14447,7 @@ Usa livello e gli &amp;attributi corrente</translatorcomment>
         <location line="-2"/>
         <location line="+2"/>
         <source>Drawing Unit</source>
-        <translation type="unfinished">Unità di misura del disegno</translation>
+        <translation>Unità di disegno</translation>
     </message>
     <message>
         <location line="-2"/>
