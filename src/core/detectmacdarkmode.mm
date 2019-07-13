915bdc891a3fbe20eb5fb6a175d7860d9e4e6873
Andrew Mustun <andrew@qcad.org>
2019-07-13 19:46:37 +0100

    update dark mode detection

--- src/core/detectmacdarkmode.mm
+++ src/core/detectmacdarkmode.mm
@@ -1,19 +1,24 @@
 #include "detectmacdarkmode.h"
+#include <QDebug>
 
 #import <AppKit/NSWindow.h>
 
 bool isMacDarkMode() {
-    NSNumber *plistRequiresAquaAppearance = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSRequiresAquaSystemAppearance"];
+    NSNumber* plistRequiresAquaAppearance = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSRequiresAquaSystemAppearance"];
     if (plistRequiresAquaAppearance==nil) {
+        qDebug() << "no value in Info.plist";
         // no value in plist:
         // detect OS dark mode:
-        NSString *osxMode = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
-        if (osxMode==nil) {
+        NSString* style = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
+        if (style==nil) {
+            qDebug() << "style is nil: no dark mode";
             return false;
         }
+        qDebug() << "style not nil: got dark mode";
         return true;
     }
     else {
+        qDebug() << "dark mode: " << ![plistRequiresAquaAppearance boolValue];
         return ![plistRequiresAquaAppearance boolValue];
     }
 }
