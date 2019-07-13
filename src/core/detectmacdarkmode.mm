a500e96c9ac5557c06f915fde5cc28ea75e163fc
Andrew Mustun <andrew@qcad.org>
2019-07-13 21:49:32 +0100

    update dark mode detection

--- src/core/detectmacdarkmode.mm
+++ src/core/detectmacdarkmode.mm
@@ -5,20 +5,33 @@
 
 bool isMacDarkMode() {
     NSNumber* plistRequiresAquaAppearance = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSRequiresAquaSystemAppearance"];
-    if (plistRequiresAquaAppearance==nil) {
-        qDebug() << "no value in Info.plist";
-        // no value in plist:
-        // detect OS dark mode:
-        NSString* style = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
-        if (style==nil) {
-            qDebug() << "style is nil: no dark mode";
+    if (plistRequiresAquaAppearance!=nil) {
+        // value found in plist file:
+        bool darkMode = ![plistRequiresAquaAppearance boolValue];
+        if (darkMode) {
+            // dark mode enabled if system is in dark mode
+            // use system setting
+            qDebug() << "dark mode on in plist file: use system setting";
+        }
+        else {
+            // dark mode explicitely disabled in plist file:
+            qDebug() << "dark mode off in plist file";
             return false;
         }
-        qDebug() << "style not nil: got dark mode";
-        return true;
     }
     else {
-        qDebug() << "dark mode: " << ![plistRequiresAquaAppearance boolValue];
-        return ![plistRequiresAquaAppearance boolValue];
+        // no value in plist file
+        // use system setting
+        qDebug() << "no dark mode value in plist file: use system setting";
     }
+
+    // no value in plist or dark mode enabled in plist:
+    // detect OS dark mode:
+    NSString* style = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
+    if (style==nil) {
+        qDebug() << "style is nil: no dark mode";
+        return false;
+    }
+    qDebug() << "style not nil: got dark mode";
+    return true;
 }
