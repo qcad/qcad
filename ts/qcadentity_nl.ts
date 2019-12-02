735334e0b027ce30126d71ddbaa125ae275a10ce
Andrew Mustun <andrew@qcad.org>
2019-12-02 20:50:06 +0100

    update translations

--- ts/qcadentity_nl.ts
+++ ts/qcadentity_nl.ts
@@ -845,7 +845,7 @@
     </message>
     <message>
         <source>Text Angle</source>
-        <translation>Teksthoek</translation>
+        <translation type="vanished">Teksthoek</translation>
     </message>
     <message>
         <source>X Scale</source>
