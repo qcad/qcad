ad117ff78aa408235d126ad39baf54d7da83494a
Andrew Mustun <info@ribbonsoft.com>
2020-05-15 07:24:01 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (105 of 105 strings)
    
    Translation: QCAD/qcadentity
    Translate-URL: http://translate.qcad.org/projects/qcad/qcadentity/fr/

--- ts/qcadentity_fr.ts
+++ ts/qcadentity_fr.ts
@@ -57,7 +57,7 @@ Indiquer la position [angle/longueur]</translatorcomment>
         <source>Width</source>
         <translatorcomment>--exact match--
 --file: qt_fr.ts--
-Largeur&#xa0;: 
+Largeur : 
 Largeur
 &amp;Largeur de page :</translatorcomment>
         <translation>Largeur</translation>
@@ -67,7 +67,7 @@ Largeur
         <source>Height</source>
         <translatorcomment>--exact match--
 --file: qt_fr.ts--
-Hauteur&#xa0;: 
+Hauteur : 
 Hauteur
 &amp;Hauteur de page :</translatorcomment>
         <translation>Hauteur</translation>
@@ -448,7 +448,7 @@ Indiquer l&apos;angle de la ligne de cote :</translatorcomment>
 &amp;Echelle
 Echelle
 Met les entités à l&apos;échelle</translatorcomment>
-        <translation>Echelle</translation>
+        <translation>Échelle</translation>
     </message>
     <message>
         <source>Simple</source>
