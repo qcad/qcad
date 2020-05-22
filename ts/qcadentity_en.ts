5476bc4915bda346450a2320f1e8b081ef70b016
Andrew Mustun <info@ribbonsoft.com>
2020-05-22 15:59:47 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (106 of 106 strings)
    
    Translation: QCAD/qcadentity
    Translate-URL: http://192.168.1.67/projects/qcad/qcadentity/en/

--- ts/qcadentity_en.ts
+++ ts/qcadentity_en.ts
@@ -591,7 +591,7 @@
     <message>
         <location line="+1"/>
         <source>Alpha</source>
-        <translation type="unfinished"></translation>
+        <translation>Alpha</translation>
     </message>
     <message>
         <location line="+2"/>
