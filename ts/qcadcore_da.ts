0674c1637ec1c813c9e38db8e790c2e99f8ac536
Test <iybtpamotm@gmx.com>
2020-06-08 07:19:50 +0000

    Translated using Weblate (Danish)
    Currently translated at 0.0% (0 of 185 strings)
    
    Translation: QCAD/qcadcore
    Translate-URL: http://translate.qcad.org/projects/qcad/qcadcore/da/

--- ts/qcadcore_da.ts
+++ ts/qcadcore_da.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/core/RUnit.cpp" line="+273"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ingen</translation>
     </message>
     <message>
         <location line="+3"/>
