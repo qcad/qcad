a9462a94257b5687bac9d17ba4db312df1e4dc31
Andrew Mustun <info@ribbonsoft.com>
2020-06-21 20:03:05 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (2319 of 2319 strings)
    
    Translation: QCAD/scripts
    Translate-URL: http://translate.qcad.org/projects/qcad/scripts/it/

--- ts/scripts_it.ts
+++ ts/scripts_it.ts
@@ -5978,12 +5978,12 @@ Punto decimale:</translatorcomment>
     <message>
         <location line="+547"/>
         <source>auto</source>
-        <translation type="unfinished">auto</translation>
+        <translation>auto</translation>
     </message>
     <message>
         <location line="+98"/>
         <source>Number of threads:</source>
-        <translation type="unfinished"></translation>
+        <translation>Numero di thread:</translation>
     </message>
     <message>
         <location line="-674"/>
@@ -15557,12 +15557,12 @@ Modificare un blocco sulla base di un riferimento di blocco scelto</translatorco
     <message>
         <location line="+2"/>
         <source>Tolerances</source>
-        <translation type="unfinished"></translation>
+        <translation>Tolleranze</translation>
     </message>
     <message>
         <location line="+0"/>
         <source>Tolerance</source>
-        <translation type="unfinished"></translation>
+        <translation>Tolleranza</translation>
     </message>
     <message>
         <location line="+2"/>
