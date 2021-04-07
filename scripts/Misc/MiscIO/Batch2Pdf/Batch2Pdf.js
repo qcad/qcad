1d5c67fd82f4de07b114db16b8999902c445f5f5
Andrew Mustun <andrew@qcad.org>
2021-04-07 11:23:37 +0200

    add Batch2Pdf script (dmitpon)

---
+++ scripts/Misc/MiscIO/Batch2Pdf/Batch2Pdf.js
@@ -0,0 +1,93 @@
+include("scripts/File/Print/Print.js");
+include("scripts/File/File.js");
+include("scripts/File/NewFile/NewFile.js");
+include("scripts/EAction.js");
+include("scripts/library.js");
+include("scripts/simple.js");
+
+function Batch2Pdf(guiAction) {
+    EAction.call(this, guiAction);
+}
+
+Batch2Pdf.prototype = new EAction();
+
+Batch2Pdf.prototype.setPdfFileName = function () {
+    var appWin = EAction.getMainWindow();
+    var fileName = this.getDocument().getFileName();
+    var initialFileName = QDir.homePath();
+    if (fileName !== "") {
+        var fileInfo = new QFileInfo(fileName);
+        initialFileName = fileInfo.absoluteFilePath();
+    }
+
+    var filterStrings = ["PDF File (*.pdf)", "PDF/A-1B File (*.pdf)"];
+    if (RSettings.getQtVersion() < 0x050A00) {
+        filterStrings = ["PDF File (*.pdf)"];
+    }
+
+    filterStrings = translateFilterStrings(filterStrings);
+
+    var ret = File.getSaveFileName(appWin, qsTr("Export to PDF"), initialFileName, filterStrings);
+
+    return ret;
+}
+
+
+Batch2Pdf.prototype.toPdf = function () {
+
+    var doc = this.getDocument();
+    var keeperCurrentBlockId = doc.getCurrentBlockId();
+
+    // Get all layout blocks Ids
+    var blockIdArrAll = doc.queryAllLayoutBlocks();
+
+    // Filter only unfrozen blocks
+    var blockIdArr = [];
+    for (i = 0; i < blockIdArrAll.length; i++) {
+        if (!doc.isBlockFrozen(blockIdArrAll[i])) {
+            blockIdArr.push(blockIdArrAll[i]);
+        }
+    }
+
+    // Set order blocks as in BlockList widget
+    blockIdArr = doc.sortBlocks(blockIdArr);
+
+    var di = new RDocumentInterface(doc);
+    var scene = new RGraphicsSceneQt(di);
+    var view = new RGraphicsViewImage();
+    view.setScene(scene);
+    var p = new Print(undefined, doc, view);
+
+    var fileName = this.setPdfFileName();
+    if (isNull(fileName)) {
+        return "No exported";
+    }
+    var printer = p.createPrinter(fileName[0]);
+    var painter = new QPainter();
+
+    painter.begin(printer);
+
+    // Print blocks
+    for (i = 0; i < blockIdArr.length; i++) {
+        doc.setCurrentBlock(blockIdArr[i]);
+        p.printCurrentBlock(printer, painter);
+        if (i < blockIdArr.length - 1) {
+            printer.newPage();
+        }
+    }
+
+    painter.end();
+    printer.destroy();
+
+    doc.setCurrentBlock(keeperCurrentBlockId);
+    return "Exported to: " + fileName[0];
+}
+
+
+Batch2Pdf.prototype.beginEvent = function () {
+    EAction.prototype.beginEvent.call(this);
+    var msg = this.toPdf();
+    // Send message to console
+    EAction.handleUserMessage(qsTr(msg));
+    this.terminate();
+}
---
+++ scripts/Misc/MiscIO/Batch2Pdf/Batch2PdfInit.js
@@ -0,0 +1,12 @@
+function init(basePath) {
+    var action = new RGuiAction(qsTranslate("Batch2Pdf", "&Batch2Pdf visible layout blocks"), RMainWindowQt.getMainWindow());
+    action.setRequiresDocument(true);
+    action.setScriptFile(basePath + "/Batch2Pdf.js");
+    action.setIcon(basePath + "/Batch2Pdf.svg");
+    action.setStatusTip(qsTranslate("Batch2Pdf", "&Batch2Pdf visible layout blocks"));
+    action.setDefaultShortcut(new QKeySequence("2,P,F"));
+    action.setDefaultCommands(["batch2pdf", "b2pdf"]);
+    action.setGroupSortOrder(52100);
+    action.setSortOrder(500);
+    action.setWidgetNames(["MiscIOMenu", "MiscIOToolBar", "MiscIOToolsPanel"]);
+}
