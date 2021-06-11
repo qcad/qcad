b92a490f55b0d9d5353bea5c81dd7a7b68fdd39a
Andrew Mustun <andrew@qcad.org>
2021-06-11 14:19:33 +0200

    add array filter polyfill

--- scripts/library.js
+++ scripts/library.js
@@ -1630,6 +1630,42 @@ Array.alphaNumericalSorter = function(a, b) {
     return RS.compareAlphanumerical(a, b);
 };
 
+Array.prototype.filter = function(func, thisArg) {
+    if ( ! ((typeof func === 'Function' || typeof func === 'function') && this) )
+        throw new TypeError();
+
+    var len = this.length >>> 0,
+        res = new Array(len), // preallocate array
+        t = this, c = 0, i = -1;
+
+    var kValue;
+    if (thisArg === undefined){
+      while (++i !== len){
+        // checks to see if the key was set
+        if (i in this){
+          kValue = t[i]; // in case t is changed in callback
+          if (func(t[i], i, t)){
+            res[c++] = kValue;
+          }
+        }
+      }
+    }
+    else{
+      while (++i !== len){
+        // checks to see if the key was set
+        if (i in this){
+          kValue = t[i];
+          if (func.call(thisArg, t[i], i, t)){
+            res[c++] = kValue;
+          }
+        }
+      }
+    }
+
+    res.length = c; // shrink down array to proper size
+    return res;
+};
+
 /**
  * Sorts this array with string items (case insensitive)
  */
