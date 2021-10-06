308b6e6738efdbe0607dba325150f3177c058ff1
Andrew Mustun <andrew@qcad.org>
2021-10-06 17:10:14 +0200

    update

--- scripts/Help/About/About.js
+++ scripts/Help/About/About.js
@@ -606,6 +606,33 @@ About.prototype.initAboutLicenses = function(textBrowser) {
           + "FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, "
           + "ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>"
 
+//    html += "<h2>pdfium</h2>"
+//    html += "<p>Copyright 2014 PDFium Authors. All rights reserved."
+//          + "<br/><br/>"
+//          + "* Redistributions of source code must retain the above copyright "
+//          + "notice, this list of conditions and the following disclaimer. "
+//          + "<br/>"
+//          + "* Redistributions in binary form must reproduce the above "
+//          + "copyright notice, this list of conditions and the following disclaimer "
+//          + "in the documentation and/or other materials provided with the "
+//          + "distribution. "
+//          + "<br/>"
+//          + "* Neither the name of Google Inc. nor the names of its "
+//          + "contributors may be used to endorse or promote products derived from "
+//          + "this software without specific prior written permission. "
+//          + "<br/><br/>"
+//          + "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "
+//          + "\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT "
+//          + "LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR "
+//          + "A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT "
+//          + "OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, "
+//          + "SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT "
+//          + "LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, "
+//          + "DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY "
+//          + "THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT "
+//          + "(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE "
+//          + "OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. ";
+
     var palettesDirs = RS.getDirectoryList("palettes");
     if (palettesDirs.length>0 && new QFileInfo(palettesDirs[0]).exists()) {
         html += "<h2>Color Palette Files</h2>"
