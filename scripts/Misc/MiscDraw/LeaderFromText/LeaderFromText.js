f27d4c8c825206c4fa3b8ae52cd3acff249c42fb
Andrew Mustun <andrew@qcad.org>
2021-12-09 21:32:37 +0100

    fix string

--- scripts/Misc/MiscDraw/LeaderFromText/LeaderFromText.js
+++ scripts/Misc/MiscDraw/LeaderFromText/LeaderFromText.js
@@ -75,6 +75,6 @@ LeaderFromText.prototype.beginEvent = function () {
     }
 
     di.applyOperation(op);
-    EAction.handleUserMessage(qsTr("LeaderFromText was created successfully"));
+    EAction.handleUserMessage(qsTr("Leader from text was created successfully"));
     this.terminate();
 };
