4a82d1e8a0bcf8f622e783b7988c899e5814c9b1
Andrew Mustun <andrew@qcad.org>
2022-02-01 11:26:00 +0100

    update

--- scripts/Misc/MiscInformation/InfoBearing/InfoBearing.js
+++ scripts/Misc/MiscInformation/InfoBearing/InfoBearing.js
@@ -1,4 +1,4 @@
-// Based on Dovetail example
+//Based on Dovetail example
 // By gmpowell71
 
 include("scripts/EAction.js");
@@ -73,11 +73,13 @@ InfoBearing.prototype.coordinateEvent = function(event) {
     switch (this.state) {
     case InfoBearing.State.SettingCorner1:
         this.corner1 = event.getModelPosition();
+        //di.setRelativeZero(this.corner1);
         this.setState(InfoBearing.State.SettingCorner2);
         break;
 
     case InfoBearing.State.SettingCorner2:
         this.corner2 = event.getModelPosition();
+        //di.setRelativeZero(this.corner2);
         var op = this.getOperation(false);
         if (!isNull(op)) {
             di.applyOperation(op);
@@ -85,37 +87,82 @@ InfoBearing.prototype.coordinateEvent = function(event) {
             
             var line = new RLine(this.corner1, this.corner2);
             var distance = line.getLength();
-            var resultMessage = qsTr("Distance:") + " " + distance;
+            var resultMessage = qsTr("Distance:") + " " + distance.toFixed(2);
             var dx1 = line.getEndPoint().x - line.getStartPoint().x;
             var dy1 = line.getEndPoint().y - line.getStartPoint().y;
             var dx = Math.abs(this.corner2.x - this.corner1.x);
             var dy = Math.abs(this.corner2.y - this.corner1.y);
 
-            var bear = undefined;
-            var az = undefined;
+           //var angle = rad2deg(line.getAngle());
+            if (dx1 > 0 && dy1 > 0){
+                var Bear = 90-rad2deg(Math.atan(dy/dx));
+                var Az = 90-rad2deg(Math.atan(dy/dx));
+                //var c1 = "\u004E";
+                //var c2 = "\u00E9";
+                var c1 = "N";
+                var c2 = "E";
 
-            if (dx1 >= 0 && dy1 >= 0){
-                bear = 90-rad2deg(Math.atan(dy/dx));
-                az = 90-rad2deg(Math.atan(dy/dx));
             }
-            if (dx1 >= 0 && dy1 < 0){
-                bear = 90-rad2deg(Math.atan(dy/dx));
-                az = 90+rad2deg(Math.atan(dy/dx));
+            if (dx1 > 0 && dy1 < 0){
+                var Bear = 90-rad2deg(Math.atan(dy/dx));
+                var Az = 90+rad2deg(Math.atan(dy/dx));
+                //var c1 = "\u004E";
+                //var c2 = "\u00E9";
+                var c1 = "S";
+                var c2 = "E";
             }
             if (dx1 < 0 && dy1 < 0){
-                bear = 90-rad2deg(Math.atan(dy/dx));
-                az = 270-rad2deg(Math.atan(dy/dx));
+                var Bear = 90-rad2deg(Math.atan(dy/dx));
+                var Az = 270-rad2deg(Math.atan(dy/dx));
+                //var c1 = "\u004E";
+                //var c2 = "\u00E9";
+                var c1 = "S";
+                var c2 = "W";
             }
-            if (dx1 < 0 && dy1 >= 0){
-                bear = 90-rad2deg(Math.atan(dy/dx));
-                az = 270+rad2deg(Math.atan(dy/dx));
+            if (dx1 < 0 && dy1 > 0){
+                var Bear = 90-rad2deg(Math.atan(dy/dx));
+                var Az = 270+rad2deg(Math.atan(dy/dx));
+                //var c1 = "\u004E";
+                //var c2 = "\u00E9";
+                var c1 = "N";
+                var c2 = "W";
+            }
+            if (dx1 == 0 && dy1 > 0){
+                var Bear = 0;
+                var Az = 0;
+                var c1 = "";
+                var c2 = " ";
+            }
+            if (dx1 == 0 && dy1 < 0){
+                var Bear = 0;
+                var Az = 180;
+                var c1 = "S";
+                var c2 = " ";
+            }
+            if (dx1 > 0 && dy1 == 0){
+                var Bear = 90;
+                var Az = 90;
+                var c1 = " ";
+                var c2 = "E";
+            }
+            if (dx1 < 0 && dy1 == 0){
+                var Bear = 90;
+                var Az = 270;
+                var c1 = " ";
+                var c2 = "W";
             }
             //var angle = rad2deg(Math.atan(dx/dy));
-            resultMessage += ",\n" + qsTr("Delta X:") + " " + dx;
-            resultMessage += ",\n" + qsTr("Delta Y:") + " " + dy;
-            resultMessage += ",\n" + qsTr("Bearing:") + " " + bear;
-            resultMessage += ",\n" + qsTr("Azimuth:") + " " + az;
-
+            var BDeg = Math.floor(Bear);
+            var BMin = Math.floor((Bear - Math.floor(Bear)) * 60);
+            var BSec = (((Bear - Math.floor(Bear)) * 60 - BMin) * 60).toFixed(2);
+            var zdeg = Math.floor(Az);
+            var zmin = Math.floor((Az - Math.floor(Az)) * 60);
+            var zsec = (((Az - Math.floor(Az)) * 60 - zmin) * 60).toFixed(2);
+            
+            resultMessage += ",\n" + qsTr("Bearing:") + " " + c1 + BDeg + "\u00B0" + BMin + "\u0022" + BSec + "\u0027" + c2 + "[" + Bear.toFixed(4) + "]";
+            resultMessage += ",\n" + qsTr("Azimuth:") + " " + zdeg + "\u00B0" + zmin + "\u0022" + zsec + "\u0027" + "[" + Az.toFixed(4) + "]";
+            //resultMessage += ",\n" + qsTr("Delta X:") + " " + dx.toFixed(2);
+            //resultMessage += ",\n" + qsTr("Delta Y:") + " " + dy.toFixed(2);
             EAction.handleUserMessage(resultMessage);
         }
         break;
@@ -140,6 +187,11 @@ InfoBearing.prototype.getOperation = function(preview) {
     var op = new RAddObjectsOperation();
     var lineData = new RLineData(this.corner1,this.corner2);
     var line = new RLineEntity(this.getDocument(), lineData);
+    //line.scale(len/20);
+    //line.move(this.corner1);
+    //line.rotate(ang, this.corner1);
+    //op.addObject(line);
+
     return op; 
 };
 
