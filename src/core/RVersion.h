48824190fb07c8cca0680e1b2d48a33759448906
Andrew Mustun <andrew@qcad.org>
2022-09-27 21:25:20 +0200

    set version to 3.27.7

--- src/core/RVersion.h
+++ src/core/RVersion.h
@@ -25,11 +25,11 @@
  * x.x.x   for releases
  * x.x.x.x for snapshots only
  */
-#define R_QCAD_VERSION_STRING "3.27.6.11"
+#define R_QCAD_VERSION_STRING "3.27.7"
 
 #define R_QCAD_VERSION_MAJOR    3
 #define R_QCAD_VERSION_MINOR    27
-#define R_QCAD_VERSION_REV      6
-#define R_QCAD_VERSION_BUILD    11
+#define R_QCAD_VERSION_REV      7
+#define R_QCAD_VERSION_BUILD    0
 
 #endif
