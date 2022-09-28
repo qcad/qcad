67d1d2c13fa65d8a8601c71c8261ed7454b18355
Andrew Mustun <andrew@qcad.org>
2022-09-28 22:52:04 +0200

    back port to Qt 4

--- src/core/RS.cpp
+++ src/core/RS.cpp
@@ -372,6 +372,7 @@ bool RS::lessThanAlphanumerical(const QString& s1, const QString& s2) {
     return RS::compareAlphanumerical(s1, s2)<0;
 }
 
+#if QT_VERSION >= 0x050300
 int RS::getPageSizeId(const QString& name) {
 
     if (name=="A0" || name=="ISO A0") {
@@ -609,6 +610,7 @@ int RS::getPageSizeId(const QString& name) {
         return QPageSize::Custom;
     }
 }
+#endif
 
 QSizeF RS::getPageSize(const QString& name) {
     if (name=="A4" || name=="ISO A4") {
