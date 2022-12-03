736e30281635bd4f967d5bc22d66467356be1224
Andrew Mustun <andrew@qcad.org>
2022-12-03 21:58:51 +0100

    add String padStart and repeat polyfills

--- scripts/library.js
+++ scripts/library.js
@@ -2265,6 +2265,54 @@ String.prototype.regexLastIndexOf = function(regex, startpos) {
     return lastIndexOf;
 };
 
+String.prototype.padStart = function(targetLength, padString) {
+    targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+    padString = String(typeof padString !== 'undefined' ? padString : ' ');
+    if (this.length > targetLength) {
+        return String(this);
+    } else {
+        targetLength = targetLength - this.length;
+        if (targetLength > padString.length) {
+            padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+        }
+        return padString.slice(0, targetLength) + String(this);
+    }
+};
+
+String.prototype.repeat = function(count) {
+    var str = '' + this;
+    // To convert string to integer.
+    count = +count;
+    // Check NaN
+    if (count != count)
+        count = 0;
+
+    if (count < 0)
+        throw new RangeError('repeat count must be non-negative');
+
+    if (count == Infinity)
+        throw new RangeError('repeat count must be less than infinity');
+
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0)
+        return '';
+
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28)
+        throw new RangeError('repeat count must not overflow maximum string size');
+
+    var maxCount = str.length * count;
+    count = Math.floor(Math.log(count) / Math.log(2));
+    while (count) {
+        str += str;
+        count--;
+    }
+    str += str.substring(0, maxCount - str.length);
+    return str;
+};
+
 /**
  * Modulo which works also for negative numbers (workaround for
  * ECMAScript modulo bug).
