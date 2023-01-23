7eb39858b92a43d33e0cac90f33cd68caf1e6c11
Andrew Mustun <andrew@qcad.org>
2023-01-23 21:46:51 +0100

    port to qt 6

--- src/entity/RAttributeData.h
+++ src/entity/RAttributeData.h
@@ -71,7 +71,14 @@ public:
         invisible = i;
     }
 
+    virtual RLinetype::Id getLinetypeId() const {
+        return RTextBasedData::getLinetypeId();
+    }
+
     virtual RLinetype::Id getLinetypeId(bool resolve, const QStack<REntity*>& blockRefStack) const;
+    virtual RLineweight::Lineweight getLineweight() const {
+        return RTextBasedData::getLineweight();
+    }
     virtual RLineweight::Lineweight getLineweight(bool resolve, const QStack<REntity*>& blockRefStack) const;
     virtual RColor getColor(bool resolve, const QStack<REntity*>& blockRefStack) const;
     virtual RColor getColor() const {
