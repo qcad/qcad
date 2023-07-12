828b6ffce343251f395933f54108212d2c0fe7d2
Andrew Mustun <andrew@qcad.org>
2023-07-12 09:53:30 +0200

    add polyfill for Array.prototype.flat

--- scripts/library.js
+++ scripts/library.js
@@ -1975,6 +1975,32 @@ Array.prototype.filter = function(func, thisArg) {
     return res;
 };
 
+Array.prototype.flat = function(depth) {
+    // If no depth is specified, default to 1
+    if (depth === undefined) {
+        depth = 1;
+    }
+
+    // Recursively reduce sub-arrays to the specified depth
+    var flatten = function (arr, depth) {
+
+        // If depth is 0, return the array as-is
+        if (depth < 1) {
+            return arr.slice();
+        }
+
+        // Otherwise, concatenate into the parent array
+        return arr.reduce(function (acc, val) {
+            return acc.concat(Array.isArray(val) ? flatten(val, depth - 1) : val);
+        }, []);
+
+    };
+
+    return flatten(this, depth);
+
+};
+
+
 /**
  * Sorts this array with string items (case insensitive)
  */
