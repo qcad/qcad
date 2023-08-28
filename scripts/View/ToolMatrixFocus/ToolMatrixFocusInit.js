e05f5a011e7d5d8c3a0ea4a4dfea28ca36a922e7
Andrew Mustun <andrew@qcad.org>
2023-08-28 16:08:09 +0200

    port to Qt 6

--- scripts/View/ToolMatrixFocus/ToolMatrixFocusInit.js
+++ scripts/View/ToolMatrixFocus/ToolMatrixFocusInit.js
@@ -1,12 +1,24 @@
 function init(basePath) {
     var action = new RGuiAction(qsTranslate("ToolMatrixFocus", "&Focus on Tool Matrix"), RMainWindowQt.getMainWindow());
     action.setScriptFile(basePath + "/ToolMatrixFocus.js");
-    if (RS.getSystemId()==="osx") {
-        action.setDefaultShortcut(new QKeySequence(Qt.ShiftModifier + Qt.MetaModifier + Qt.Key_Space));
+
+    if (RSettings.getQtVersion()>=0x060000) {
+        if (RS.getSystemId()==="osx") {
+            action.setDefaultShortcut(new QKeySequence(new QKeyCombination(Qt.ShiftModifier|Qt.MetaModifier, Qt.Key_Space)));
+        }
+        else {
+            action.setDefaultShortcut(new QKeySequence(new QKeyCombination(Qt.ShiftModifier|Qt.ControlModifier, Qt.Key_Space)));
+        }
     }
     else {
-        action.setDefaultShortcut(new QKeySequence(Qt.ShiftModifier + Qt.ControlModifier + Qt.Key_Space));
+        if (RS.getSystemId()==="osx") {
+            action.setDefaultShortcut(new QKeySequence(Qt.ShiftModifier + Qt.MetaModifier + Qt.Key_Space));
+        }
+        else {
+            action.setDefaultShortcut(new QKeySequence(Qt.ShiftModifier + Qt.ControlModifier + Qt.Key_Space));
+        }
     }
+
     action.setNoState();
     action.checkable = false;
     action.setGroupSortOrder(3900);
