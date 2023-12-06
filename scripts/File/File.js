f8374a4a41f3b5c5ae2f3f507065b1291a804acd
Andrew Mustun <andrew@qcad.org>
2023-12-06 15:24:21 +0100

    avoid warning when using default filter

--- scripts/File/File.js
+++ scripts/File/File.js
@@ -302,7 +302,7 @@ File.getOpenFileName = function(parentWidget, caption, dir, filterStrings, noAll
  * \param version String from the file version drop down in the save as dialog.
  */
 File.isRecommendedVersion = function(version) {
-    if (!version.contains("*.dxf") && !version.contains("*.dwg")) {
+    if (version.length>0 && !version.contains("*.dxf") && !version.contains("*.dwg")) {
         // only dxf and dwg are recommended to save drawing files:
         return false;
     }
