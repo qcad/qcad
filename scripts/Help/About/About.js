0e25039ec8e486d1e1ffada741499e48eb5a9da1
Andrew Mustun <andrew@qcad.org>
2024-04-08 16:18:44 +0200

    add qtjsapi license

--- scripts/Help/About/About.js
+++ scripts/Help/About/About.js
@@ -495,7 +495,7 @@ About.prototype.initAboutSystem = function(textEdit) {
     text += "\nNegative sign: " + sysloc.negativeSign();
     text += "\nPositive sign: " + sysloc.positiveSign();
     text += "\nText direction: " + sysloc.textDirection();
-    if (RSettings.getQtVersion() > 0x060000) {
+    if (RSettings.getQtVersion() >= 0x060000) {
         // TODO #qt6: prints number not name
         text += "\nSystem codec: " + QStringConverter.nameForEncoding(QStringConverter.System);
     }
@@ -545,6 +545,28 @@ About.prototype.initAboutLicenses = function(textBrowser) {
             + "<body>"
             + "<h1>%1</h1>".arg(qsTr("Licenses"))
             + "<hr/>";
+
+    if (RSettings.getQtVersion() >= 0x060000) {
+        html += "<h2>qtjsapi</h2>"
+        html += "<p>Copyright 2024 RibbonSoft GmbH</p>"
+              + "<p>Permission is hereby granted, free of charge, to any person obtaining a "
+              + "copy of this software and associated documentation files (the “Software”), "
+              + "to deal in the Software without restriction, including without limitation "
+              + "the rights to use, copy, modify, merge, publish, distribute, sublicense, "
+              + "and/or sell copies of the Software, and to permit persons to whom the "
+              + "Software is furnished to do so, subject to the following conditions:"
+              + "<br/><br/>"
+              + "The above copyright notice and this permission notice shall be included in "
+              + "all copies or substantial portions of the Software."
+              + "<br/><br/>"
+              + "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR "
+              + "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, "
+              + "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE "
+              + "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
+              + "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING "
+              + "FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER "
+              + "DEALINGS IN THE SOFTWARE.</p>";
+    }
 
     html += "<h2>libspatialindex</h2>"
     html += "<p>Permission is hereby granted, free of charge, to any person obtaining a "
