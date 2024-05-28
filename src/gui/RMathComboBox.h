213cb9989efb6130e7f09946ee57db2237cf4e3f
Andrew Mustun <andrew@qcad.org>
2024-05-28 08:18:13 +0200

    clean up

--- src/gui/RMathComboBox.h
+++ src/gui/RMathComboBox.h
@@ -121,19 +121,9 @@ public slots:
         // forward signal from RMathLineEdit:
         emit valueChanged(value, error);
     }
-    //void slotEnterKeyPressed() {
-    //    qDebug() << "slotEnterKeyPressed";
-    //    // forward signal from RMathLineEdit:
-    //    emit enterKeyPressed();
-    //}
 
 signals:
     void valueChanged(double value, const QString& error);
-    //void enterKeyPressed();
-
-//protected:
-    //virtual void keyPressEvent(QKeyEvent* event);
-    //virtual void keyReleaseEvent(QKeyEvent* event);
 };
 
 Q_DECLARE_METATYPE(RMathComboBox*)
