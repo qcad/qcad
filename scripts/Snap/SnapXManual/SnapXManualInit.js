2a7394d884e5fd55a51d5a0e2d090df5bd4f50b4
Andrew Mustun <andrew@qcad.org>
2024-06-12 16:35:51 +0200

    change shortcuts with . to avoid conflict with line edits

--- scripts/Snap/SnapXManual/SnapXManualInit.js
+++ scripts/Snap/SnapXManual/SnapXManualInit.js
@@ -2,8 +2,8 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("SnapXManual", "&X/Y from Points"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/SnapXManual.js");
-    action.setDefaultShortcut(new QKeySequence(".,x"));
-    action.setDefaultCommands([".x"]);
+    action.setDefaultShortcut(new QKeySequence("p,x"));
+    action.setDefaultCommands(["px"]);
     action.setOverride();
     action.setGroupSortOrder(15550);
     action.setSortOrder(100);
--- scripts/Snap/SnapYManual/SnapYManualInit.js
+++ scripts/Snap/SnapYManual/SnapYManualInit.js
@@ -2,8 +2,8 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("SnapYManual", "&Y/X from Points"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/SnapYManual.js");
-    action.setDefaultShortcut(new QKeySequence(".,y"));
-    action.setDefaultCommands([".y"]);
+    action.setDefaultShortcut(new QKeySequence("p,y"));
+    action.setDefaultCommands(["py"]);
     action.setOverride();
     action.setGroupSortOrder(15550);
     action.setSortOrder(200);
