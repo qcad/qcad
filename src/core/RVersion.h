eb0cb9067698bc677c42cd683d6bf3df9beb6307
Andrew Mustun <andrew@qcad.org>
2024-11-12 10:33:21 +0100

    set version to 3.31.2.3

--- src/core/RVersion.h
+++ src/core/RVersion.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011-2023 by Andrew Mustun. All rights reserved.
+ * Copyright (c) 2011-2024 by Andrew Mustun. All rights reserved.
  * 
  * This file is part of the QCAD project.
  *
@@ -25,11 +25,11 @@
  * x.x.x   for releases
  * x.x.x.x for snapshots only
  */
-#define R_QCAD_VERSION_STRING "3.31.2.2"
+#define R_QCAD_VERSION_STRING "3.31.2.3"
 
 #define R_QCAD_VERSION_MAJOR    3
 #define R_QCAD_VERSION_MINOR    31
 #define R_QCAD_VERSION_REV      2
-#define R_QCAD_VERSION_BUILD    2
+#define R_QCAD_VERSION_BUILD    3
 
 #endif
