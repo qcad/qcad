b954a0ec243223f951454034b70dbc16f0a061d4
Andrew Mustun <andrew@qcad.org>
2024-12-06 17:03:40 +0100

    use destrDialog to delete dialog

--- scripts/Draw/Text/Text.js
+++ scripts/Draw/Text/Text.js
@@ -58,9 +58,7 @@ Text.prototype.beginEvent = function() {
 
     var dialog = new TextDialog(this.mode);
     this.textData = dialog.show();
-    if (RSettings.getQtVersion() >= 0x060000) {
-        delete dialog;
-    }
+    destrDialog(dialog);
 
     if (isNull(this.textData)) {
         this.terminate();
