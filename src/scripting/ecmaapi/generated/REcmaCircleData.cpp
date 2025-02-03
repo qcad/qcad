f35126e42d17b222b9f2636a19df64576a998098
Andrew Mustun <andrew@qcad.org>
2025-02-03 12:40:34 +0100

    update script bindings

--- src/scripting/ecmaapi/generated/REcmaCircleData.cpp
+++ src/scripting/ecmaapi/generated/REcmaCircleData.cpp
@@ -9,6 +9,10 @@
         
                 #include "RDocument.h"
             
+                #include "RLine.h"
+            
+                #include "RPolyline.h"
+            
             
         // includes for base ecma wrapper classes
         
