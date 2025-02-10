6f931d6e293f13963878dc0938751af6a4005235
qcad <andrew@qcad.org>
2025-02-10 09:29:49 +0000

    port to gcc

--- src/core/RTransaction.cpp
+++ src/core/RTransaction.cpp
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with QCAD.
  */
+#include <typeinfo>
+
 #include "RBlock.h"
 #include "RBlockReferenceEntity.h"
 #include "RDocument.h"
