37c39080d8dd39ec15f4a66b92269254ae0196a2
Andrew Mustun <andrew@qcad.org>
2025-05-23 14:14:06 +0200

    update

--- scripts/Help/VideoTutorials/VideoTutorialsInit.js
+++
@@ -1,9 +0,0 @@
-function init(basePath) {
-    var action = new RGuiAction(qsTranslate("VideoTutorials", "QCAD &Video Tutorials"), RMainWindowQt.getMainWindow());
-    action.setRequiresDocument(false);
-    action.setScriptFile(basePath + "/VideoTutorials.js");
-    action.setNoState();
-    action.setGroupSortOrder(110200);
-    action.setSortOrder(150);
-    action.setWidgetNames(["HelpMenu", "!HelpToolBar"]);
-}
