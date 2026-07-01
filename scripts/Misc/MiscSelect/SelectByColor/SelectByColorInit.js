796435c873fe7b6e5f7dfeaec72ddd9be85d04c3
Andrew Mustun <andrew@qcad.org>
2026-07-01 20:29:54 +0200

    fix string translations in *Init.js files

--- scripts/Misc/MiscSelect/SelectByColor/SelectByColorInit.js
+++ scripts/Misc/MiscSelect/SelectByColor/SelectByColorInit.js
@@ -2,7 +2,7 @@ function init(basePath) {
     var action = new RGuiAction(qsTranslate("SelectByColor", "By Color"), RMainWindowQt.getMainWindow());
     action.setRequiresDocument(true);
     action.setScriptFile(basePath + "/SelectByColor.js");
-    action.setStatusTip(qsTr("Select all objects of a color"));
+    action.setStatusTip(qsTranslate("SelectByColor", "Select all objects of a color"));
     action.setDefaultShortcut(new QKeySequence("t,f"));
     action.setGroupSortOrder(53100);
     action.setSortOrder(200);
