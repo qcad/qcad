ef21c1ef2905f30164bf492e3d2fee4f06035696
Andrew Mustun <andrew@qcad.org>
2026-09-17 09:02:09 +0200

    bind RSnapSelection

--- src/scripting/ecmaapi/RScriptHandlerEcma.cpp
+++ src/scripting/ecmaapi/RScriptHandlerEcma.cpp
@@ -336,6 +336,7 @@
 #include "REcmaSnapPerpendicular.h"
 #include "REcmaSnapReference.h"
 #include "REcmaSnapRestriction.h"
+#include "REcmaSnapSelection.h"
 #include "REcmaSnapTangential.h"
 #include "REcmaSolidData.h"
 #include "REcmaSolidEntity.h"
@@ -752,6 +753,7 @@ RScriptHandlerEcma::RScriptHandlerEcma() : engine(NULL), debugger(NULL) {
     REcmaSnapOnEntity::initEcma(*engine);
     REcmaSnapPerpendicular::initEcma(*engine);
     REcmaSnapReference::initEcma(*engine);
+    REcmaSnapSelection::initEcma(*engine);
     REcmaSnapTangential::initEcma(*engine);
 
     REcmaSnapRestriction::initEcma(*engine);
